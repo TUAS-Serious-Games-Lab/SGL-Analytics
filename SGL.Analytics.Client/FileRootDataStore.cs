88e3633654971abb403ebe48cf31ba39180497db
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-11 16:03:19 +0200

    Implemented basic functionality of FileRootDataStore.

--- SGL.Analytics.Client/FileRootDataStore.cs
+++ SGL.Analytics.Client/FileRootDataStore.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Client {
 	public class FileRootDataStore : IRootDataStore {
+		private struct StorageStructure {
+			public Guid? UserID;
+		}
+		private StorageStructure storage;
 		string appName;
+		JsonSerializerOptions jsonOptions = new JsonSerializerOptions() {
+			WriteIndented = true
+		};
 
-		public Guid? UserID { get; set; }
-
-		public string DataDirectory => throw new NotImplementedException();
+		public Guid? UserID { get => storage.UserID; set => storage.UserID = value; }
 
 		public FileRootDataStore(string appName) {
 			this.appName = appName;
+			LoadAsync().Wait();
 		}
 
-		public Task SaveAsync() {
-			throw new NotImplementedException();
+		private async Task LoadAsync() {
+			var file = GetStorageFile();
+			if (!File.Exists(file)) {
+				// No saved state found, use default state.
+				return;
+			}
+			await using (var storageFileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None, bufferSize: 4096, useAsync: true)) {
+				storage = await JsonSerializer.DeserializeAsync<StorageStructure>(storageFileStream, jsonOptions);
+			}
+		}
+
+		public string DataDirectory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
+
+		private string GetStorageFile() {
+			return Path.Combine(DataDirectory, "AppDataStore.json");
+		}
+
+		public async Task SaveAsync() {
+			await using (var storageFileStream = new FileStream(GetStorageFile(), FileMode.Open, FileAccess.Read, FileShare.None, bufferSize: 4096, useAsync: true)) {
+				await JsonSerializer.SerializeAsync<StorageStructure>(storageFileStream, storage, jsonOptions);
+			}
 		}
 	}
 }
