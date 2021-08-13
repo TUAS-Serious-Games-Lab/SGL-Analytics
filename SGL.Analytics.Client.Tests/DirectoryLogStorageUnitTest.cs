5757d856a77f91da45ac9d7f8eb6cd2f06cb7e7a
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-13 12:04:48 +0200

    Added TestCase CreatedLogFileIsEnumerated as the first unit test for DirectoryLogStorage.

---
+++ SGL.Analytics.Client.Tests/DirectoryLogStorageUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SGL.Analytics.Client.Tests {
+	public class DirectoryLogStorageUnitTest {
+		private DirectoryLogStorage storage;
+		public DirectoryLogStorageUnitTest() {
+			string directory = Path.Combine(Directory.GetCurrentDirectory(), "UnitTestTemp", "DirectoryLogStorageUnitTest");
+			Directory.CreateDirectory(directory);
+			storage = new DirectoryLogStorage(directory);
+		}
+		[Fact]
+		public void CreatedLogFileIsEnumerated() {
+			ILogStorage.ILogFile? metadata;
+			using (var stream = storage.CreateLogFile(out metadata)) { }
+			Assert.Contains(metadata, storage.EnumerateLogs());
+		}
+	}
+}
