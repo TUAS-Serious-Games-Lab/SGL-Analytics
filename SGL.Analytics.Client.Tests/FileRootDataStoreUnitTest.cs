0c1b90f063ccd570a173661f9c5ce4795f004193
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-13 16:17:29 +0200

    Added unit tests file for FileRootDataStore.

---
+++ SGL.Analytics.Client.Tests/FileRootDataStoreUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SGL.Analytics.Client.Tests {
+	public class FileRootDataStoreUnitTest : IDisposable {
+		private const string appName = "FileRootDataStoreUnitTest";
+		private FileRootDataStore getDS() => new FileRootDataStore(appName);
+		public void Dispose() {
+			string filename;
+			FileRootDataStore temp = getDS();
+			filename = temp.StorageFile;
+			File.Delete(filename);
+		}
+
+
+	}
+}
