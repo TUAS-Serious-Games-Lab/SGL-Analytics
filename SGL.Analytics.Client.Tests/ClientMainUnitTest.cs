d9f0d263e6fce3da976c5b00fe89a8b2c444c02f
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-16 16:11:18 +0200

    Added test objects to ClientMainUnitTest.

--- SGL.Analytics.Client.Tests/ClientMainUnitTest.cs
+++ SGL.Analytics.Client.Tests/ClientMainUnitTest.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SGL.Analytics.Client.Tests {
-	class ClientMainUnitTest {
+	public class ClientMainUnitTest : IDisposable {
+
+		private InMemoryLogStorage storage = new InMemoryLogStorage();
+		private FakeRootDataStore ds = new FakeRootDataStore();
+		private SGLAnalytics analytics;
+
+		public ClientMainUnitTest() {
+			analytics = new SGLAnalytics("SGLAnalyticsUnitTests", "FakeApiKey", ds, storage);
+		}
+
+		public void Dispose() {
+			storage.Dispose();
+		}
+
 	}
 }
