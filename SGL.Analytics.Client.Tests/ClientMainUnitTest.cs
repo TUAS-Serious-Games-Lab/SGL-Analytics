60d3370df3c471c6e02e3f3de886fb544771b789
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-17 15:42:05 +0200

    Added test case RecordedEntriesAreWrittenToTheCorrectLogFile for client API main class.

--- SGL.Analytics.Client.Tests/ClientMainUnitTest.cs
+++ SGL.Analytics.Client.Tests/ClientMainUnitTest.cs
@@ -334,5 +334,108 @@ namespace SGL.Analytics.Client.Tests {
 				Assert.Equal(JsonValueKind.Null, testObjectC.ValueKind);
 			}
 		}
+
+		public class SimpleTestEvent {
+			public string Name { get; set; } = "";
+		}
+		[Fact]
+		public async Task RecordedEntriesAreWrittenToTheCorrectLogFile() {
+			analytics.StartNewLog();
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test A" });
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test B" });
+			analytics.RecordEventUnshared("Channel 2", new SimpleTestEvent { Name = "Test C" });
+			analytics.RecordSnapshotUnshared("Channel 3", 1, "Snap A");
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test D" });
+			analytics.RecordSnapshotUnshared("Channel 3", 1, "Snap B");
+			analytics.RecordSnapshotUnshared("Channel 3", 2, "Snap C");
+
+			analytics.StartNewLog();
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test E" });
+			analytics.RecordEventUnshared("Channel 2", new SimpleTestEvent { Name = "Test F" });
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test G" });
+			analytics.RecordSnapshotUnshared("Channel 3", 1, "Snap D");
+			analytics.RecordEventUnshared("Channel 2", new SimpleTestEvent { Name = "Test H" });
+			analytics.RecordEventUnshared("Channel 2", new SimpleTestEvent { Name = "Test I" });
+			analytics.RecordSnapshotUnshared("Channel 3", 2, "Snap E");
+
+			analytics.StartNewLog();
+			analytics.RecordEventUnshared("Channel 1", new SimpleTestEvent { Name = "Test J" });
+			analytics.RecordEventUnshared("Channel 2", new SimpleTestEvent { Name = "Test K" });
+			analytics.RecordSnapshotUnshared("Channel 3", 1, "Snap F");
+
+			await analytics.FinishAsync();
+
+			foreach (var logFile in storage.EnumerateLogs()) {
+				output.WriteLine("");
+				output.WriteLine($"{logFile.ID}:");
+				output.WriteLogContents(logFile);
+			}
+
+			static void readAndAssertSimpleTestEvent(ref JsonElement.ArrayEnumerator arrEnumerator, string expChannel, string expName) {
+				Assert.True(arrEnumerator.MoveNext());
+				var entryElem = arrEnumerator.Current;
+				Assert.True(entryElem.TryGetProperty("Channel", out var actChannel));
+				Assert.Equal(expChannel, actChannel.GetString());
+				Assert.True(entryElem.TryGetProperty("EntryType", out var actEntryType));
+				Assert.Equal("Event", actEntryType.GetString());
+				Assert.True(entryElem.TryGetProperty("EventType", out var actEventType));
+				Assert.Equal("SimpleTestEvent", actEventType.GetString());
+				Assert.True(entryElem.TryGetProperty("Payload", out var payload));
+				Assert.True(payload.TryGetProperty("Name", out var actName));
+				Assert.Equal(expName, actName.GetString());
+			}
+
+			static void readAndAssertSimpleSnapshot(ref JsonElement.ArrayEnumerator arrEnumerator, string expChannel, int expObjectId, string expPayload) {
+				Assert.True(arrEnumerator.MoveNext());
+				var entryElem = arrEnumerator.Current;
+				Assert.True(entryElem.TryGetProperty("Channel", out var actChannel));
+				Assert.Equal(expChannel, actChannel.GetString());
+				Assert.True(entryElem.TryGetProperty("EntryType", out var actEntryType));
+				Assert.Equal("Snapshot", actEntryType.GetString());
+				Assert.True(entryElem.TryGetProperty("ObjectID", out var actObjectIdElem));
+				Assert.True(actObjectIdElem.TryGetInt32(out var actObjectId));
+				Assert.Equal(expObjectId, actObjectId);
+				Assert.True(entryElem.TryGetProperty("Payload", out var payload));
+				Assert.Equal(expPayload, payload.GetString());
+			}
+
+			var logs = storage.EnumerateLogs().GetEnumerator();
+			Assert.True(logs.MoveNext());
+			await using (var stream = logs.Current.OpenRead()) {
+				using (var jsonDoc = await JsonDocument.ParseAsync(stream)) {
+					var arrEnumerator = jsonDoc.RootElement.EnumerateArray().GetEnumerator();
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test A");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test B");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 2", "Test C");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 1, "Snap A");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test D");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 1, "Snap B");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 2, "Snap C");
+				}
+			}
+			Assert.True(logs.MoveNext());
+			await using (var stream = logs.Current.OpenRead()) {
+				using (var jsonDoc = await JsonDocument.ParseAsync(stream)) {
+					var arrEnumerator = jsonDoc.RootElement.EnumerateArray().GetEnumerator();
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test E");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 2", "Test F");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test G");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 1, "Snap D");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 2", "Test H");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 2", "Test I");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 2, "Snap E");
+				}
+			}
+			Assert.True(logs.MoveNext());
+			await using (var stream = logs.Current.OpenRead()) {
+				using (var jsonDoc = await JsonDocument.ParseAsync(stream)) {
+					var arrEnumerator = jsonDoc.RootElement.EnumerateArray().GetEnumerator();
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 1", "Test J");
+					readAndAssertSimpleTestEvent(ref arrEnumerator, "Channel 2", "Test K");
+					readAndAssertSimpleSnapshot(ref arrEnumerator, "Channel 3", 1, "Snap F");
+				}
+			}
+			Assert.False(logs.MoveNext());
+		}
 	}
 }
