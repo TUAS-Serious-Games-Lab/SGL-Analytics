56056dcab92f8d2acb29519b4037b99e30df56a2
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-18 13:11:37 +0200

    Specified LogMetadataDTO.
    See issue #10

--- SGL.Analytics.DTO/LogMetadataDTO.cs
+++ SGL.Analytics.DTO/LogMetadataDTO.cs
@@ -4,5 +4,11 @@ using System.Text;
 
 namespace SGL.Analytics.DTO {
 	public class LogMetadataDTO {
+		public string AppName { get; set; } = "";
+		public Guid UserId { get; set; }
+		public Guid LogFileId { get; set; }
+		public DateTime CreationTime { get; set; }
+		public DateTime EndTime { get; set; }
+
 	}
 }
