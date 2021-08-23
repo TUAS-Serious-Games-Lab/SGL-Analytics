a4674a11c6f7e78f47aae26a4e5a51562052b3f9
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-23 10:24:40 +0200

    Defined attributes for UserRegistrationDTO.
    See issue #10

--- SGL.Analytics.DTO/UserRegistrationDTO.cs
+++ SGL.Analytics.DTO/UserRegistrationDTO.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace SGL.Analytics.DTO {
-	public record UserRegistrationDTO();
+	public record UserRegistrationDTO(string appName, string Username, Dictionary<string, object?> StudySpecificAttributes);
 }
