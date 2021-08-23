0dfa33f6c0c7201d1b906078e6a543dacca1bc16
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-23 10:39:15 +0200

    Map BaseUserData (including derived types) to the corresponding UserRegistrationDTO.

--- SGL.Analytics.Client/UserData.cs
+++ SGL.Analytics.Client/UserData.cs
@@ -1,9 +1,20 @@
+using SGL.Analytics.Client.Utilities;
+using SGL.Analytics.DTO;
+
 namespace SGL.Analytics.Client {
 	public class BaseUserData {
 		public string Username { get; set; }
 
 		public BaseUserData(string username) {
 			Username = username;
+		}
+
+		public UserRegistrationDTO MakeDTO(string appName) {
+			// Study-specific data are intended to be kept in derived classes.
+			// => Map all properties of dynamic type to a dictionary for transmission.
+			var studySpecificProperties = DictionaryDataMapping.ToDataMappingDictionary(this);
+			studySpecificProperties.Remove("Username");
+			return new UserRegistrationDTO(appName, Username, studySpecificProperties);
 		}
 	}
 }
