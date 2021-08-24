5e97e4bfed6527f121cfce67843df4058a213d47
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-24 19:25:45 +0200

    Added UploadTime and LocalLogId to LogMetadata.
    UploadTime stores the time (server clock) when the log file was uploaded to the LogCollector service.
    LocalLogId is the LogId GUID given by the client on upload. It should usually be identical to the normal Id.
    	However, if there is an id conflict with a log from another user, the LogCollector my need to assign a different id to the log.
    	In this case, the normal Id is the newly generated one and LocalLogId is the one as passed by the client.

--- SGL.Analytics.Backend.Model/LogMetadata.cs
+++ SGL.Analytics.Backend.Model/LogMetadata.cs
@@ -11,17 +11,22 @@ namespace SGL.Analytics.Backend.Model {
 		public int AppId { get; set; }
 		public Application App { get; set; }
 		public Guid UserId { get; set; }
+		public Guid LocalLogId { get; set; }
 
 		public DateTime CreationTime { get; set; }
 		public DateTime EndTime { get; set; }
+		public DateTime UploadTime { get; set; }
 
-		public LogMetadata(Guid id, int appId, Application app, Guid userId, DateTime creationTime, DateTime endTime) {
+		public LogMetadata(Guid id, int appId, Application app, Guid userId,
+			Guid localLogId, DateTime creationTime, DateTime endTime, DateTime uploadTime) {
 			Id = id;
 			AppId = appId;
 			App = app;
 			UserId = userId;
+			LocalLogId = localLogId;
 			CreationTime = creationTime;
 			EndTime = endTime;
+			UploadTime = uploadTime;
 		}
 	}
 }
