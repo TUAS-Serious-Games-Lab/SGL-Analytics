3c4d94d36b96f56fdb9011a5c66e1d4728f1c9a8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-24 18:29:00 +0200

    Defined DB model classes for LogCollector service.

--- SGL.Analytics.Backend.Model/LogMetadata.cs
+++ SGL.Analytics.Backend.Model/LogMetadata.cs
@@ -1,9 +1,27 @@
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SGL.Analytics.Backend.Model {
+	[Index(nameof(AppId))]
+	[Index(nameof(AppId), nameof(UserId))]
 	public class LogMetadata {
 		[Key]
 		public Guid Id { get; set; }
+		public int AppId { get; set; }
+		public Application App { get; set; }
+		public Guid UserId { get; set; }
+
+		public DateTime CreationTime { get; set; }
+		public DateTime EndTime { get; set; }
+
+		public LogMetadata(Guid id, int appId, Application app, Guid userId, DateTime creationTime, DateTime endTime) {
+			Id = id;
+			AppId = appId;
+			App = app;
+			UserId = userId;
+			CreationTime = creationTime;
+			EndTime = endTime;
+		}
 	}
 }
