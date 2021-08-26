5a664b07da8ce83495ae835ec0e265f50a894d38
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-26 13:40:44 +0200

    Added test case CreatedLogsAreCorrectlyEnumeratedForAppUser as unit test for FileSystemCollectorLogStorage.

--- SGL.Analytics.Backend.LogCollector.Tests/FileSystemCollectorLogStorageUnitTest.cs
+++ SGL.Analytics.Backend.LogCollector.Tests/FileSystemCollectorLogStorageUnitTest.cs
@@ -89,5 +89,31 @@ namespace SGL.Analytics.Backend.LogCollector.Tests {
 			LogPath logPathB = new LogPath { AppName = appName + "_B", UserId = logPathA.UserId, LogId = logPathA.LogId, Suffix = suffix };
 			await separationTest(logPathA, logPathB);
 		}
+
+		[Fact]
+		public async Task CreatedLogsAreCorrectlyEnumeratedForAppUser() {
+			Guid userId = Guid.NewGuid();
+			var positivePathList = new List<LogPath>() {
+				new LogPath() { AppName = appName, UserId = userId, LogId = Guid.NewGuid(), Suffix = suffix },
+				new LogPath() { AppName = appName, UserId = userId, LogId = Guid.NewGuid(), Suffix = suffix },
+				new LogPath() { AppName = appName, UserId = userId, LogId = Guid.NewGuid(), Suffix = suffix },
+				new LogPath() { AppName = appName, UserId = userId, LogId = Guid.NewGuid(), Suffix = suffix }
+			};
+			var negativePathList = new List<LogPath>() {
+				new LogPath() { AppName = appName + "_A", UserId = userId, LogId = Guid.NewGuid(), Suffix = suffix },
+				new LogPath() { AppName = appName + "_B", UserId = Guid.NewGuid(), LogId = Guid.NewGuid(), Suffix = suffix },
+				new LogPath() { AppName = appName, UserId = Guid.NewGuid(), LogId = Guid.NewGuid(), Suffix = suffix }
+			};
+			using (var content = new MemoryStream()) {
+				foreach (var p in positivePathList) {
+					await storage.StoreLogAsync(p, content);
+				}
+				foreach (var p in negativePathList) {
+					await storage.StoreLogAsync(p, content);
+				}
+			}
+			Assert.All(positivePathList, p => Assert.Contains(p, storage.EnumerateLogs(appName, userId)));
+			Assert.All(negativePathList, p => Assert.DoesNotContain(p, storage.EnumerateLogs(appName, userId)));
+		}
 	}
 }
