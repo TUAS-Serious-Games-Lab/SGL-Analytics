dd6ba39fc6df787d1ad2e22653de09c38f2ce322
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-27 18:11:39 +0200

    Added Application layer for user management in backend.

---
+++ SGL.Analytics.Backend.Users.Application/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Application.Model {
+	public class User {
+	}
+}
