2b7830ac6d90290bb94a45619d17dfc0f321795c
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-30 15:17:23 +0200

    Implemented DbApplicationRepository.

---
+++ SGL.Analytics.Backend.Logs.Infrastructure/Services/DbApplicationRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGL.Analytics.Backend.Logs.Application.Interfaces;
+using SGL.Analytics.Backend.Logs.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Logs.Infrastructure.Services {
+	public class DbApplicationRepository : IApplicationRepository, IDisposable, IAsyncDisposable {
+		private LogsContext context;
+
+		public DbApplicationRepository(LogsContext context) {
+			this.context = context;
+		}
+
+		public void Dispose() => context.Dispose();
+		public ValueTask DisposeAsync() => context.DisposeAsync();
+
+		public Task<Domain.Entity.Application?> GetApplicationByNameAsync(string appName) {
+			return context.Applications.Where(a => a.Name == appName).SingleOrDefaultAsync<Domain.Entity.Application?>();
+		}
+
+		public async Task<Domain.Entity.Application> AddApplicationAsync(Domain.Entity.Application app) {
+			await context.Applications.AddAsync(app);
+			await context.SaveChangesAsync();
+			return app;
+		}
+	}
+}
