b74428d9a9d045db9edf45280f80a0839d414339
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-30 16:28:01 +0200

    Added ApplicationDoesNotExistException to indicate when a log upload request was made with an application name that does not exist in the database.

--- SGL.Analytics.Backend.Logs.Application/Interfaces/IApplicationRepository.cs
+++ SGL.Analytics.Backend.Logs.Application/Interfaces/IApplicationRepository.cs
@@ -5,6 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.Logs.Application.Interfaces {
+	public class ApplicationDoesNotExistException : Exception {
+		public ApplicationDoesNotExistException(string appName, Exception? innerException = null) : base($"The given application with name '{appName}' does not exist.", innerException) {
+			AppName = appName;
+		}
+
+		public string AppName { get; set; }
+	}
+
 	public interface IApplicationRepository {
 		Task<Domain.Entity.Application?> GetApplicationByNameAsync(string appName);
 		Task<Domain.Entity.Application> AddApplicationAsync(Domain.Entity.Application app);
