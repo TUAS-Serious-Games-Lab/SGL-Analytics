bab50d9967e37f56e2ad70c09b9c405728c5fed0
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-30 13:58:49 +0200

    Added test case MetadataEntryIsCorrectlyUpdatedInDb for DbLogMetadataRepository.

--- SGL.Analytics.Backend.Logs.Infrastructure.Tests/DbLogMetadataRepositoryUnitTest.cs
+++ SGL.Analytics.Backend.Logs.Infrastructure.Tests/DbLogMetadataRepositoryUnitTest.cs
@@ -44,6 +44,40 @@ namespace SGL.Analytics.Backend.Logs.Infrastructure.Tests {
 			Assert.Equal(logMd.UploadTime, logMdRead?.UploadTime);
 		}
 
+		[Fact]
+		public async Task MetadataEntryIsCorrectlyUpdatedInDb() {
+			var logId = Guid.NewGuid();
+			var logMd = new LogMetadata(logId, 0, Guid.NewGuid(), logId, DateTime.Now.AddMinutes(-15), DateTime.Now.AddMinutes(-1), DateTime.Now.AddSeconds(-30));
+			var app = new Domain.Entity.Application(0, "DbLogMetadataRepositoryUnitTest", "FakeApiToken");
+			await using (var context = createContext()) {
+				context.Applications.Add(app);
+				logMd.App = app;
+				await using (var repo = new DbLogMetadataRepository(context)) {
+					logMd = await repo.AddLogMetadataAsync(logMd);
+				}
+			}
+			LogMetadata? logMd2;
+			await using (var repo = new DbLogMetadataRepository(createContext())) {
+				logMd2 = await repo.GetLogMetadataByIdAsync(logId);
+				logMd2.LocalLogId = Guid.NewGuid();
+				logMd2.UploadTime = DateTime.Now;
+				await repo.UpdateLogMetadataAsync(logMd2);
+			}
+			LogMetadata? logMdRead;
+			await using (var repo = new DbLogMetadataRepository(createContext())) {
+				logMdRead = await repo.GetLogMetadataByIdAsync(logId);
+			}
+			Assert.NotNull(logMdRead);
+			Assert.Equal(logId, logMdRead?.Id);
+			Assert.Equal(logMd2.Id, logMdRead?.Id);
+			Assert.Equal(logMd2.AppId, logMdRead?.AppId);
+			Assert.Equal(logMd2.UserId, logMdRead?.UserId);
+			Assert.Equal(logMd2.LocalLogId, logMdRead?.LocalLogId);
+			Assert.Equal(logMd2.CreationTime, logMdRead?.CreationTime);
+			Assert.Equal(logMd2.EndTime, logMdRead?.EndTime);
+			Assert.Equal(logMd2.UploadTime, logMdRead?.UploadTime);
+		}
+
 		public void Dispose() {
 			testDb.Dispose();
 		}
