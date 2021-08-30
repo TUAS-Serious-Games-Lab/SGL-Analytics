0aaca0215b7789a7658cbaaf5195e72148c5b719
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-30 18:43:56 +0200

    Implemented DummyApplicationRepository for test decoupling.

---
+++ SGL.Analytics.Backend.Logs.Application.Tests/Dummies/DummyApplicationRepository.cs
@@ -0,0 +1,31 @@
+﻿using SGL.Analytics.Backend.Logs.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Logs.Application.Tests.Dummies {
+	public class DummyApplicationRepository : IApplicationRepository {
+		private int nextId = 1;
+		private readonly Dictionary<string, Domain.Entity.Application> apps = new();
+
+		public async Task<Domain.Entity.Application> AddApplicationAsync(Domain.Entity.Application app) {
+			await Task.CompletedTask;
+			if (apps.ContainsKey(app.Name)) throw new InvalidOperationException($"An application with the given name '{app.Name}' is already present.");
+			app.Id = nextId++;
+			apps.Add(app.Name, app);
+			return app;
+		}
+
+		public async Task<Domain.Entity.Application?> GetApplicationByNameAsync(string appName) {
+			await Task.CompletedTask;
+			if (apps.TryGetValue(appName, out var app)) {
+				return app;
+			}
+			else {
+				return null;
+			}
+		}
+	}
+}
