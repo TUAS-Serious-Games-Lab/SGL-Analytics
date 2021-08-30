6d29e662f199d220285d580b57dab5a18cade847
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-30 18:44:57 +0200

    Implemented DummyLogMetadataRepository for test decoupling.

---
+++ SGL.Analytics.Backend.Logs.Application.Tests/Dummies/DummyLogMetadataRepository.cs
@@ -0,0 +1,38 @@
+﻿using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Logs.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Logs.Application.Tests.Dummies {
+	public class DummyLogMetadataRepository : ILogMetadataRepository {
+		private Dictionary<Guid, LogMetadata> logs = new();
+
+		public async Task<LogMetadata> AddLogMetadataAsync(LogMetadata logMetadata) {
+			await Task.CompletedTask;
+			if (logs.ContainsKey(logMetadata.Id)) throw new InvalidOperationException($"An log metadata entry with the given id '{logMetadata.Id}' is already present.");
+			logs.Add(logMetadata.Id, logMetadata);
+			return logMetadata;
+		}
+
+		public async Task<LogMetadata?> GetLogMetadataByIdAsync(Guid logId) {
+			await Task.CompletedTask;
+			if (logs.TryGetValue(logId, out var logMd)) {
+				return logMd;
+			}
+			else {
+				return null;
+			}
+		}
+
+		public async Task<LogMetadata> UpdateLogMetadataAsync(LogMetadata logMetadata) {
+			await Task.CompletedTask;
+			Debug.Assert(logs.ContainsKey(logMetadata.Id));
+			logs[logMetadata.Id] = logMetadata;
+			return logMetadata;
+		}
+	}
+}
