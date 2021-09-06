511e99c824cc2af66c115d444fdea3f90139d993
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-06 17:26:32 +0200

    Implemented DbUserRepository.

---
+++ SGL.Analytics.Backend.Users.Infrastructure/Services/DbUserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Users.Application.Interfaces;
+using SGL.Analytics.Backend.Users.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Infrastructure.Services {
+	public class DbUserRepository : IUserRepository {
+		private UsersContext context;
+
+		public DbUserRepository(UsersContext context) {
+			this.context = context;
+		}
+
+		public async Task<UserRegistration?> GetUserByIdAsync(Guid id) {
+			return await context.UserRegistrations
+				.Include(u => u.App).ThenInclude(a => a.UserProperties)
+				.Include(u => u.AppSpecificProperties).ThenInclude(p => p.Definition)
+				.Where(u => u.Id == id)
+				.SingleOrDefaultAsync<UserRegistration?>();
+		}
+
+		public async Task<UserRegistration> RegisterUserAsync(UserRegistration userReg) {
+			userReg.ValidateProperties(); // Throws on error
+			context.UserRegistrations.Add(userReg);
+			await context.SaveChangesAsync();
+			return userReg;
+		}
+	}
+}
