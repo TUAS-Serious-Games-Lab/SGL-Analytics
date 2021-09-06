85f1188ea0ed623a0abdc32274a64e981d2aa82e
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-06 18:46:44 +0200

    Added test case ApplicationWithPropertiesCanBeCreatedAndRetrievedWithPropertiesPreseved as unit test for DbApplicationRepository for user API service.

--- SGL.Analytics.Backend.Users.Infrastructure.Tests/DbApplicationRepositoryUnitTest.cs
+++ SGL.Analytics.Backend.Users.Infrastructure.Tests/DbApplicationRepositoryUnitTest.cs
@@ -41,5 +41,60 @@ namespace SGL.Analytics.Backend.Users.Infrastructure.Tests {
 			Assert.Equal(app2.Name, appRead?.Name);
 			Assert.Equal(app2.ApiToken, appRead?.ApiToken);
 		}
+
+		[Fact]
+		public async Task ApplicationWithPropertiesCanBeCreatedAndRetrievedWithPropertiesPreseved() {
+			var appOrig = new ApplicationWithUserProperties(Guid.NewGuid(), "DbApplicationRepositoryUnitTest", StringGenerator.GenerateRandomWord(32));
+			appOrig.UserProperties = new List<ApplicationUserPropertyDefinition>();
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestInt", UserPropertyType.Integer, true));
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestFP", UserPropertyType.FloatingPoint, false));
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestString", UserPropertyType.String, true));
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestDateTime", UserPropertyType.DateTime, false));
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestGuid", UserPropertyType.Guid, true));
+			appOrig.UserProperties.Add(new ApplicationUserPropertyDefinition(0, appOrig.Id, "TestJson", UserPropertyType.Json, false));
+			await using (var context = createContext()) {
+				var repo = new DbApplicationRepository(context);
+				await repo.AddApplicationAsync(appOrig);
+			}
+			ApplicationWithUserProperties? appRead;
+			await using (var context = createContext()) {
+				var repo = new DbApplicationRepository(context);
+				appRead = await repo.GetApplicationByNameAsync("DbApplicationRepositoryUnitTest");
+			}
+			Assert.NotNull(appRead);
+			Assert.Equal(appOrig.Id, appRead?.Id);
+			Assert.Equal(appOrig.Name, appRead?.Name);
+			Assert.Equal(appOrig.ApiToken, appRead?.ApiToken);
+
+			var intProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestInt");
+			Assert.Equal(appRead?.Id, intProp.AppId);
+			Assert.Equal(UserPropertyType.Integer, intProp.Type);
+			Assert.True(intProp.Required);
+
+			var fpProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestFP");
+			Assert.Equal(appRead?.Id, fpProp.AppId);
+			Assert.Equal(UserPropertyType.FloatingPoint, fpProp.Type);
+			Assert.False(fpProp.Required);
+
+			var strProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestString");
+			Assert.Equal(appRead?.Id, strProp.AppId);
+			Assert.Equal(UserPropertyType.String, strProp.Type);
+			Assert.True(strProp.Required);
+
+			var dtProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestDateTime");
+			Assert.Equal(appRead?.Id, dtProp.AppId);
+			Assert.Equal(UserPropertyType.DateTime, dtProp.Type);
+			Assert.False(dtProp.Required);
+
+			var guidProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestGuid");
+			Assert.Equal(appRead?.Id, guidProp.AppId);
+			Assert.Equal(UserPropertyType.Guid, guidProp.Type);
+			Assert.True(guidProp.Required);
+
+			var jsonProp = Assert.Single(appRead?.UserProperties, pd => pd.Name == "TestJson");
+			Assert.Equal(appRead?.Id, jsonProp.AppId);
+			Assert.Equal(UserPropertyType.Json, jsonProp.Type);
+			Assert.False(jsonProp.Required);
+		}
 	}
 }
