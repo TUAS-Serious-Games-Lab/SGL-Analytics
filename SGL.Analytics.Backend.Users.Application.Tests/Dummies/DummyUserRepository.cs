678a4c3375ceb303318bcc0be94306e8dbfadd2e
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-07 19:33:08 +0200

    Implemented DummyUserRepository for UserManager unit test decoupling.

---
+++ SGL.Analytics.Backend.Users.Application.Tests/Dummies/DummyUserRepository.cs
@@ -0,0 +1,40 @@
+﻿using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Domain.Exceptions;
+using SGL.Analytics.Backend.Users.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Application.Tests.Dummies {
+	public class DummyUserRepository : IUserRepository {
+		private readonly Dictionary<Guid, UserRegistration> users = new Dictionary<Guid, UserRegistration>();
+
+		public async Task<UserRegistration?> GetUserByIdAsync(Guid id) {
+			await Task.CompletedTask;
+			if (users.TryGetValue(id, out var user)) {
+				return user;
+			}
+			else {
+				return null;
+			}
+		}
+
+		public async Task<UserRegistration> RegisterUserAsync(UserRegistration userReg) {
+			await Task.CompletedTask;
+			if (userReg.Id == Guid.Empty) userReg.Id = Guid.NewGuid();
+			if (users.ContainsKey(userReg.Id)) throw new EntityUniquenessConflictException("UserRegistration", "Id");
+			if (users.Values.Any(u => u.Username == userReg.Username)) throw new EntityUniquenessConflictException("UserRegistration", "Username");
+			users.Add(userReg.Id, userReg);
+			return userReg;
+		}
+
+		public async Task<UserRegistration> UpdateUserAsync(UserRegistration userReg) {
+			await Task.CompletedTask;
+			Debug.Assert(users.ContainsKey(userReg.Id));
+			users[userReg.Id] = userReg;
+			return userReg;
+		}
+	}
+}
