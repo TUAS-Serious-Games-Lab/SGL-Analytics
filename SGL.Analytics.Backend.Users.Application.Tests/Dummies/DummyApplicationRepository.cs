ba4ba3fc43e6e68d09ae328646ac03c7d024cd6d
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-07 19:32:09 +0200

    Implemented DummyApplicationRepository for UserManager unit test decoupling.

---
+++ SGL.Analytics.Backend.Users.Application.Tests/Dummies/DummyApplicationRepository.cs
@@ -0,0 +1,32 @@
+﻿using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Domain.Exceptions;
+using SGL.Analytics.Backend.Users.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Application.Tests.Dummies {
+	public class DummyApplicationRepository : IApplicationRepository {
+		private readonly Dictionary<string, ApplicationWithUserProperties> apps = new();
+
+		public async Task<ApplicationWithUserProperties> AddApplicationAsync(ApplicationWithUserProperties app) {
+			await Task.CompletedTask;
+			if (apps.ContainsKey(app.Name)) throw new EntityUniquenessConflictException("Application", "Name");
+			if (app.Id == Guid.Empty) app.Id = Guid.NewGuid();
+			if (apps.Values.Any(a => a.Id == app.Id)) throw new EntityUniquenessConflictException("Application", "Id");
+			apps.Add(app.Name, app);
+			return app;
+		}
+
+		public async Task<ApplicationWithUserProperties?> GetApplicationByNameAsync(string appName) {
+			await Task.CompletedTask;
+			if (apps.TryGetValue(appName, out var app)) {
+				return app;
+			}
+			else {
+				return null;
+			}
+		}
+	}
+}
