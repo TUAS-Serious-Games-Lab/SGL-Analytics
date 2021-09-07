efbe235fcf639338d0c54c7eefa82148907550e8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-07 18:57:27 +0200

    Added error mapping checks for uniqueness violations and DbUpdateConcurrencyException.

--- SGL.Analytics.Backend.Users.Infrastructure/Services/DbApplicationRepository.cs
+++ SGL.Analytics.Backend.Users.Infrastructure/Services/DbApplicationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Domain.Exceptions;
 using SGL.Analytics.Backend.Users.Application.Interfaces;
 using SGL.Analytics.Backend.Users.Infrastructure.Data;
 using System;
@@ -18,7 +19,24 @@ namespace SGL.Analytics.Backend.Users.Infrastructure.Services {
 
 		public async Task<ApplicationWithUserProperties> AddApplicationAsync(ApplicationWithUserProperties app) {
 			context.Applications.Add(app);
-			await context.SaveChangesAsync();
+			try {
+				await context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException ex) {
+				throw new ConcurrencyConflictException(ex);
+			}
+			catch (DbUpdateException ex) {
+				// Should happen rarely and unfortunately, at the time of writing, there is no portable way (between databases) of further classifying the error.
+				// To check if ex is a unique constraint violation, we would need to inspect its inner exception and switch over exception types for all supported providers and their internal error classifications.
+				// To avoid this coupling, rather pay the perf cost of querrying again in this rare case.
+				if (context.Applications.Count(a => a.Name == app.Name) > 0) {
+					throw new EntityUniquenessConflictException("Application", "Name", ex);
+				}
+				else if (context.Applications.Count(a => a.Id == app.Id) > 0) {
+					throw new EntityUniquenessConflictException("Application", "Id", ex);
+				}
+				else throw;
+			}
 			return app;
 		}
 
