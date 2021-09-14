f3e830cf5344befbdc6c0a054d2160ee8472c5e4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-14 18:51:42 +0200

    Added test case OwnerAuthorizationDeniesAccessForNonOwnerBasedOnRouteParam for OwnerAuthorizationHandler in combination with JWT authentication and the [Authorize] attribute.

--- SGL.Analytics.Backend.Security.Tests/OwnerAuthorizationIntegrationTest.cs
+++ SGL.Analytics.Backend.Security.Tests/OwnerAuthorizationIntegrationTest.cs
@@ -94,5 +94,25 @@ namespace SGL.Analytics.Backend.Security.Tests {
 				}
 			}
 		}
+
+		[Theory]
+		[InlineData("user1")]
+		[InlineData("user2")]
+		[InlineData("user3")]
+		[InlineData("user4")]
+		[InlineData("owner1")]
+		[InlineData("owner2")]
+		[InlineData("owner3")]
+		[InlineData("owner4")]
+		public async Task OwnerAuthorizationDeniesAccessForNonOwnerBasedOnRouteParam(string subRoute) {
+			var ownerId = Guid.NewGuid();
+			var token = fixture.TokenGenerator.GenerateToken(Guid.NewGuid(), TimeSpan.FromMinutes(5));
+			using (var client = fixture.CreateClient()) {
+				var request = new HttpRequestMessage(HttpMethod.Get, $"/api/OwnerAuthorizationTest/{subRoute}/{ownerId}");
+				request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+				var response = await client.SendAsync(request);
+				Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+			}
+		}
 	}
 }
