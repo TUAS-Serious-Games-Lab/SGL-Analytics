55e899180213c628c6ad76949646063f4991f81d
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-15 19:39:59 +0200

    Added test case UserRegistrationWithMissingRequiredPropertyFailsWithExpectedError as unit test for AnalyticsUserController.

--- SGL.Analytics.Backend.Users.Registration.Tests/AnalyticsUserControllerUnitTest.cs
+++ SGL.Analytics.Backend.Users.Registration.Tests/AnalyticsUserControllerUnitTest.cs
@@ -110,5 +110,16 @@ namespace SGL.Analytics.Backend.Users.Registration.Tests {
 			Assert.Equal(StatusCodes.Status400BadRequest, res.StatusCode);
 			Assert.IsType<string>(res.Value);
 		}
+		[Fact]
+		public async Task UserRegistrationWithMissingRequiredPropertyFailsWithExpectedError() {
+			Dictionary<string, object?> props = new Dictionary<string, object?> { ["Bar"] = "Hello" };
+			var userRegDTO = new UserRegistrationDTO(appName, "Testuser",
+				StringGenerator.GenerateRandomWord(16),// Not cryptographic, but ok for test
+				props);
+			var result = await controller.RegisterUser(appApiToken, userRegDTO);
+			var res = Assert.IsType<BadRequestObjectResult>(result.Result);
+			Assert.Equal(StatusCodes.Status400BadRequest, res.StatusCode);
+			Assert.IsType<string>(res.Value);
+		}
 	}
 }
