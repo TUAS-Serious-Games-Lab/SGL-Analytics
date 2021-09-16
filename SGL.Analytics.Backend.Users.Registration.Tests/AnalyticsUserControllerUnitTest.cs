ab5d42fadffa222f8f37dafc6f4a79ed5731daaf
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-16 11:16:02 +0200

    Added test case LoginWithUnmatchingAppAndUserFailsWithExpectedError as unit test for AnalyticsUserController.

--- SGL.Analytics.Backend.Users.Registration.Tests/AnalyticsUserControllerUnitTest.cs
+++ SGL.Analytics.Backend.Users.Registration.Tests/AnalyticsUserControllerUnitTest.cs
@@ -191,5 +191,19 @@ namespace SGL.Analytics.Backend.Users.Registration.Tests {
 			var loginResult = await controller.Login(loginRequest);
 			Assert.Equal(StatusCodes.Status401Unauthorized, Assert.IsType<UnauthorizedObjectResult>(loginResult.Result).StatusCode);
 		}
+		[Fact]
+		public async Task LoginWithUnmatchingAppAndUserFailsWithExpectedError() {
+			var appName2 = "AnalyticsUserControllerUnitTest_2";
+			var appApiToken2 = StringGenerator.GenerateRandomWord(32);
+			await userManager.AddApplicationAsync(ApplicationWithUserProperties.Create(appName2, appApiToken2));
+			var secret = StringGenerator.GenerateRandomWord(16);// Not cryptographic, but ok for test
+			Dictionary<string, object?> props = new Dictionary<string, object?> { ["Foo"] = "Test", ["Bar"] = "Hello" };
+			var userRegDTO = new UserRegistrationDTO(appName, "Testuser", secret, props);
+			var regResult = await controller.RegisterUser(appApiToken, userRegDTO);
+			var userId = ((regResult.Result as ObjectResult)?.Value as UserRegistrationResultDTO)?.UserId ?? throw new XunitException("Registration failed.");
+			var loginRequest = new LoginRequestDTO(appName2, appApiToken2, userId, secret);
+			var loginResult = await controller.Login(loginRequest);
+			Assert.Equal(StatusCodes.Status401Unauthorized, Assert.IsType<UnauthorizedObjectResult>(loginResult.Result).StatusCode);
+		}
 	}
 }
