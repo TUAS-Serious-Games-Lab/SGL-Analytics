4b43cf8ab5dd2f805a680eab7a6d880135ef4cb3
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-16 11:17:39 +0200

    Created test fixture for UserRegistrationIntegrationTest.

---
+++ SGL.Analytics.Backend.Users.Registration.Tests/UserRegistrationIntegrationTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Analytics.Backend.Security;
+using SGL.Analytics.Backend.TestUtilities;
+using SGL.Analytics.Backend.Users.Infrastructure.Data;
+using SGL.Analytics.Utilities;
+using SGL.Analytics.TestUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SGL.Analytics.Backend.Users.Registration.Tests {
+	public class UserRegistrationIntegrationTestFixture : DbWebAppIntegrationTestFixtureBase<UsersContext, Startup> {
+		public readonly string AppName = "UserRegistrationIntegrationTest";
+		public string AppApiToken { get; } = StringGenerator.GenerateRandomWord(32);
+		public JwtOptions JwtOptions { get; } = new JwtOptions() {
+			Audience = "UserRegistrationIntegrationTest",
+			Issuer = "UserRegistrationIntegrationTest",
+			SymmetricKey = "TestingS3cr3tTestingS3cr3t"
+		};
+		public Dictionary<string, string> JwtConfig { get; }
+
+		public ITestOutputHelper? Output { get; set; } = null;
+		public JwtTokenGenerator TokenGenerator { get; }
+
+		public UserRegistrationIntegrationTestFixture() {
+			JwtConfig = new() {
+				["Jwt:Audience"] = JwtOptions.Audience,
+				["Jwt:Issuer"] = JwtOptions.Issuer,
+				["Jwt:SymmetricKey"] = JwtOptions.SymmetricKey,
+			};
+			TokenGenerator = new JwtTokenGenerator(JwtOptions.Issuer, JwtOptions.Audience, JwtOptions.SymmetricKey);
+		}
+
+		protected override void SeedDatabase(UsersContext context) {
+			var app = ApplicationWithUserProperties.Create(AppName, AppApiToken);
+			app.AddProperty("Foo", UserPropertyType.String, true);
+			app.AddProperty("Bar", UserPropertyType.String);
+			context.Applications.Add(app);
+			context.SaveChanges();
+		}
+
+		protected override IHostBuilder CreateHostBuilder() {
+			return base.CreateHostBuilder().ConfigureAppConfiguration(config => config.AddInMemoryCollection(JwtConfig))
+				.ConfigureLogging(logging => logging.AddXUnit(() => Output).SetMinimumLevel(LogLevel.Trace));
+		}
+	}
+
+	public class UserRegistrationIntegrationTest : IClassFixture<UserRegistrationIntegrationTestFixture> {
+		private UserRegistrationIntegrationTestFixture fixture;
+		private ITestOutputHelper output;
+
+		public UserRegistrationIntegrationTest(UserRegistrationIntegrationTestFixture fixture, ITestOutputHelper output) {
+			this.fixture = fixture;
+			this.output = output;
+			this.fixture.Output = output;
+		}
+	}
+}
