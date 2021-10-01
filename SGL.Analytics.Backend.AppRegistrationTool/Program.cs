49b128d74b6614d1961119f3bad47806b164e4b2
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-01 17:10:17 +0200

    Added configuration and service registration for AppRegistrationTool.

--- SGL.Analytics.Backend.AppRegistrationTool/Program.cs
+++ SGL.Analytics.Backend.AppRegistrationTool/Program.cs
@@ -1,5 +1,13 @@
 ﻿using CommandLine;
 using CommandLine.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SGL.Analytics.Backend.Logs.Infrastructure;
+using SGL.Analytics.Backend.Logs.Infrastructure.Data;
+using SGL.Analytics.Backend.Users.Infrastructure;
+using SGL.Analytics.Backend.Users.Infrastructure.Data;
+using SGL.Analytics.Backend.WebUtilities;
 using SGL.Analytics.Utilities;
 using System;
 using System.Collections.Generic;
@@ -8,20 +16,25 @@ using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.AppRegistrationTool {
 	class Program {
+		class BaseOptions {
+			[Option('c', "config", HelpText = "Specifies an additional config file to use.")]
+			public IEnumerable<string> ConfigFiles { get; set; } = new List<string>();
+		}
+		[Verb("generate-api-token", HelpText = "Generates an API token for an application registration.")]
+		class GenerateApiTokenOptions : BaseOptions { }
 		[Verb("push", HelpText = "Push application registration data into backend. Registers new application, adds new user registration properties.")]
-		class PushOptions {
-
+		class PushOptions : BaseOptions {
+			[Value(0, MetaName = "APP_DEFINITIONS_DIRECTORY", HelpText = "(Default: Current Directory) The directory in which there is one json file for each application to push.")]
+			public string AppDefinitionsDirectory { get; set; } = Environment.CurrentDirectory;
 		}
 		[Verb("remove-property", HelpText = "(Not yet implemented) Remove a user registration property from an application registration and all its instances.")]
-		class RemovePropertyOptions {
+		class RemovePropertyOptions : BaseOptions {
 			// Not implemented
 		}
 		[Verb("remove-application", HelpText = "(Not yet implemented) Removes an application AND ALL ASSOCIATED DATABASE ENTRIES (user registrations, property definitions, app log metadata).")]
-		class RemoveApplicationOptions {
+		class RemoveApplicationOptions : BaseOptions {
 			// Not implemented
 		}
-		[Verb("generate-api-token", HelpText = "Generates an API token for an application registration.")]
-		class GenerateApiTokenOptions { }
 		async static Task<int> Main(string[] args) => await ((Func<ParserResult<object>, Task<int>>)(res => res.MapResult(
 			async (PushOptions opts) => await PushMain(opts),
 			async (GenerateApiTokenOptions opts) => await GenerateApiTokenMain(opts),
@@ -40,9 +53,30 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 			}));
 			return 1;
 		}
+
+		static IHostBuilder CreateHostBuilder(PushOptions opts) =>
+			 Host.CreateDefaultBuilder()
+					.UseConsoleLifetime()
+					.ConfigureAppConfiguration(config => {
+						foreach (var configFile in opts.ConfigFiles) {
+							config.AddJsonFile(configFile);
+						}
+					})
+					.ConfigureServices((context, services) => {
+						services.UseUsersBackendInfrastructure(context.Configuration);
+						services.UseLogsBackendInfrastructure(context.Configuration);
+					});
+
 		async static Task<int> PushMain(PushOptions opts) {
+			using var host = CreateHostBuilder(opts).Build();
+
+			await host.WaitForDbsReadyAsync<LogsContext, UsersContext>(pollingInterval: TimeSpan.FromMilliseconds(100));
+			using (var scope = host.Services.CreateScope()) {
+
+			}
 			return 0;
 		}
+
 		async static Task<int> GenerateApiTokenMain(GenerateApiTokenOptions opts) {
 			var token = SecretGenerator.Instance.GenerateSecret(32);
 			await Console.Out.WriteLineAsync(token);
