6941182e63f5696548d42c0d804db50e04cb9db0
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-28 18:52:35 +0200

    Added API docs for DTO types.

--- SGL.Analytics.DTO/LoginRequestDTO.cs
+++ SGL.Analytics.DTO/LoginRequestDTO.cs
@@ -5,5 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.DTO {
+	/// <summary>
+	/// Specifies the data transferred from the client to the server when the client attempts to login a user.
+	/// </summary>
 	public record LoginRequestDTO(string AppName, string AppApiToken, Guid UserId, string UserSecret);
 }
