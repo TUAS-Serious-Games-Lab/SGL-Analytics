31f753335eecddba48eaaa2f0013bf8987cd09f9
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-29 17:38:37 +0200

    Made SGL.Analytics.Client.UserData.MakeDTO internal becasue it is not intended to be part of the public API.

--- SGL.Analytics.Client/UserData.cs
+++ SGL.Analytics.Client/UserData.cs
@@ -9,7 +9,7 @@ namespace SGL.Analytics.Client {
 			Username = username;
 		}
 
-		public UserRegistrationDTO MakeDTO(string appName, string secret) {
+		internal UserRegistrationDTO MakeDTO(string appName, string secret) {
 			// Study-specific data are intended to be kept in derived classes.
 			// => Map all properties of dynamic type to a dictionary for transmission.
 			var studySpecificProperties = DictionaryDataMapping.ToDataMappingDictionary(this);
