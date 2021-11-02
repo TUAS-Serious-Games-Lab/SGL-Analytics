4edaba65b6518e8814f7a86fc323aa18c9f0a2e6
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-11-02 18:43:00 +0100

    Added API docs for IApplicationRepository.

--- SGL.Analytics.Backend.Users.Application/Interfaces/IApplicationRepository.cs
+++ SGL.Analytics.Backend.Users.Application/Interfaces/IApplicationRepository.cs
@@ -7,9 +7,30 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.Users.Application.Interfaces {
+	/// <summary>
+	/// Specifies the interface for a repository to store <see cref="ApplicationWithUserProperties"/> objects.
+	/// </summary>
 	public interface IApplicationRepository {
+		/// <summary>
+		/// Asynchronously obtains the application with the given name if it exists.
+		/// </summary>
+		/// <param name="appName">The unique technical name of the application.</param>
+		/// <param name="ct">A cancellation token to allow cancelling the operation.</param>
+		/// <returns>A task object representing the operation, providing the following result: The application object if the application exists, or <see langword="null"/> otherwise.</returns>
 		Task<ApplicationWithUserProperties?> GetApplicationByNameAsync(string appName, CancellationToken ct = default);
+		/// <summary>
+		/// Asynchronously creates the given application object in the repository.
+		/// </summary>
+		/// <param name="app">The application data for the application to create.</param>
+		/// <param name="ct">A cancellation token to allow cancelling the operation.</param>
+		/// <returns>A task object representing the operation, providing the created object as its result.</returns>
 		Task<ApplicationWithUserProperties> AddApplicationAsync(ApplicationWithUserProperties app, CancellationToken ct = default);
+		/// <summary>
+		/// Asynchronously updates the given application object in the repository.
+		/// </summary>
+		/// <param name="app">The updated application data.</param>
+		/// <param name="ct">A cancellation token to allow cancelling the operation.</param>
+		/// <returns>A task object representing the operation, providing the updated object as its result.</returns>
 		Task<ApplicationWithUserProperties> UpdateApplicationAsync(ApplicationWithUserProperties app, CancellationToken ct = default);
 	}
 }
