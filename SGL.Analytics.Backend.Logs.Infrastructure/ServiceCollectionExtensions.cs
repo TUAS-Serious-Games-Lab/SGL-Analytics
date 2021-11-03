87442282d6163ecc832d01c8f2ab7df559fd03d8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-11-03 11:29:04 +0100

    Added API docs for ServiceCollectionExtensions.

--- SGL.Analytics.Backend.Logs.Infrastructure/ServiceCollectionExtensions.cs
+++ SGL.Analytics.Backend.Logs.Infrastructure/ServiceCollectionExtensions.cs
@@ -11,7 +11,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.Logs.Infrastructure {
+	/// <summary>
+	/// Provides the <see cref="UseLogsBackendInfrastructure(IServiceCollection, IConfiguration)"/> extension method.
+	/// </summary>
 	public static class ServiceCollectionExtensions {
+		/// <summary>
+		/// Adds the infrastructure services classes for the SGL Analytics logs collector backend service.
+		/// </summary>
+		/// <param name="services">The service collection to add to.</param>
+		/// <param name="config">The root config object to obtain configuration entries from.</param>
+		/// <returns>A reference to <paramref name="services"/> for chaining.</returns>
 		public static IServiceCollection UseLogsBackendInfrastructure(this IServiceCollection services, IConfiguration config) {
 			services.AddDbContext<LogsContext>(options => options.UseNpgsql(config.GetConnectionString("LogsContext")));
 
--- SGL.Analytics.Backend.Users.Infrastructure/ServiceCollectionExtensions.cs
+++ SGL.Analytics.Backend.Users.Infrastructure/ServiceCollectionExtensions.cs
@@ -11,7 +11,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.Users.Infrastructure {
+	/// <summary>
+	/// Provides the <see cref="UseUsersBackendInfrastructure(IServiceCollection, IConfiguration)"/> extension method.
+	/// </summary>
 	public static class ServiceCollectionExtensions {
+		/// <summary>
+		/// Adds the infrastructure services classes for the SGL Analytics user registration backend service.
+		/// </summary>
+		/// <param name="services">The service collection to add to.</param>
+		/// <param name="config">The root config object to obtain configuration entries from.</param>
+		/// <returns>A reference to <paramref name="services"/> for chaining.</returns>
 		public static IServiceCollection UseUsersBackendInfrastructure(this IServiceCollection services, IConfiguration config) {
 			services.AddDbContext<UsersContext>(options => options.UseNpgsql(config.GetConnectionString("UsersContext")));
 
