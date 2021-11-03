084d50bb276f12a3867507a859ab1f30ab5ea0f3
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-11-03 11:30:59 +0100

    Added API docs for DB migrations.

--- SGL.Analytics.Backend.Logs.Infrastructure/Migrations/20210920155909_InitialCreate.cs
+++ SGL.Analytics.Backend.Logs.Infrastructure/Migrations/20210920155909_InitialCreate.cs
@@ -1,74 +1,71 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace SGL.Analytics.Backend.Logs.Infrastructure.Migrations
-{
-    public partial class InitialCreate : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Applications",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
-                    ApiToken = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Applications", x => x.Id);
-                });
+namespace SGL.Analytics.Backend.Logs.Infrastructure.Migrations {
+	/// <summary>
+	/// Creates the initial database schema for the logs collector database.
+	/// </summary>
+	public partial class InitialCreate : Migration {
+		/// <inheritdoc/>
+		protected override void Up(MigrationBuilder migrationBuilder) {
+			migrationBuilder.CreateTable(
+				name: "Applications",
+				columns: table => new {
+					Id = table.Column<Guid>(type: "uuid", nullable: false),
+					Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+					ApiToken = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
+				},
+				constraints: table => {
+					table.PrimaryKey("PK_Applications", x => x.Id);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "LogMetadata",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    AppId = table.Column<Guid>(type: "uuid", nullable: false),
-                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
-                    LocalLogId = table.Column<Guid>(type: "uuid", nullable: false),
-                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
-                    EndTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
-                    UploadTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
-                    FilenameSuffix = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
-                    Complete = table.Column<bool>(type: "boolean", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_LogMetadata", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_LogMetadata_Applications_AppId",
-                        column: x => x.AppId,
-                        principalTable: "Applications",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "LogMetadata",
+				columns: table => new {
+					Id = table.Column<Guid>(type: "uuid", nullable: false),
+					AppId = table.Column<Guid>(type: "uuid", nullable: false),
+					UserId = table.Column<Guid>(type: "uuid", nullable: false),
+					LocalLogId = table.Column<Guid>(type: "uuid", nullable: false),
+					CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+					EndTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+					UploadTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+					FilenameSuffix = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+					Complete = table.Column<bool>(type: "boolean", nullable: false)
+				},
+				constraints: table => {
+					table.PrimaryKey("PK_LogMetadata", x => x.Id);
+					table.ForeignKey(
+						name: "FK_LogMetadata_Applications_AppId",
+						column: x => x.AppId,
+						principalTable: "Applications",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+				});
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Applications_Name",
-                table: "Applications",
-                column: "Name",
-                unique: true);
+			migrationBuilder.CreateIndex(
+				name: "IX_Applications_Name",
+				table: "Applications",
+				column: "Name",
+				unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_LogMetadata_AppId",
-                table: "LogMetadata",
-                column: "AppId");
+			migrationBuilder.CreateIndex(
+				name: "IX_LogMetadata_AppId",
+				table: "LogMetadata",
+				column: "AppId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_LogMetadata_AppId_UserId",
-                table: "LogMetadata",
-                columns: new[] { "AppId", "UserId" });
-        }
+			migrationBuilder.CreateIndex(
+				name: "IX_LogMetadata_AppId_UserId",
+				table: "LogMetadata",
+				columns: new[] { "AppId", "UserId" });
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "LogMetadata");
+		/// <inheritdoc/>
+		protected override void Down(MigrationBuilder migrationBuilder) {
+			migrationBuilder.DropTable(
+				name: "LogMetadata");
 
-            migrationBuilder.DropTable(
-                name: "Applications");
-        }
-    }
+			migrationBuilder.DropTable(
+				name: "Applications");
+		}
+	}
 }
