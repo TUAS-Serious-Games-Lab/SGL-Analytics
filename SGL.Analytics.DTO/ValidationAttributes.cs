c36d4942caa4dc7e52321866be4d90c2f68a99d1
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-12-13 20:01:58 +0100

    Added API docs for PlainNameAttribute.

--- SGL.Analytics.DTO/ValidationAttributes.cs
+++ SGL.Analytics.DTO/ValidationAttributes.cs
@@ -2,10 +2,21 @@
 using System.Linq;
 
 namespace SGL.Analytics.DTO {
+	/// <summary>
+	/// Specifies that the property carrying this attribute must be a string, must not contain special characters outside of letters, digits, brackets (round, square, curly), '-', '.', or '_', and must not contain multiple adjacent '.'s.
+	/// </summary>
 	public class PlainNameAttribute : ValidationAttribute {
+		/// <summary>
+		/// Constructs the attribute object using the default error message.
+		/// </summary>
 		public PlainNameAttribute() : base("{0} must be a string, must not contain special characters outside of letters, digits, brackets (round, square, curly), '-', '.', or '_', and must not contain multiple adjacent '.'s.") { }
+		/// <summary>
+		/// Constructs the attribute object using the given custom error message.
+		/// </summary>
+		/// <param name="errorMessage">A custom error message template, <c>{0}</c> is formated as the <see cref="ValidationContext.DisplayName"/>.</param>
 		public PlainNameAttribute(string errorMessage) : base(errorMessage) { }
 
+		/// <inheritdoc/>
 		protected override ValidationResult IsValid(object? value, ValidationContext validationContext) {
 			if (value == null) return ValidationResult.Success!;
 			if (!(value is string strVal)) return FailValidation(validationContext);
