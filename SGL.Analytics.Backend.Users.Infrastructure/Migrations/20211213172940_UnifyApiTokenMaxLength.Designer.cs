585926f8d31a016deda0aa428212588cc22a15c1
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-12-13 18:33:51 +0100

    Fixed length of App API token db column in users service.

--- SGL.Analytics.Backend.Users.Infrastructure/Migrations/20211213172940_UnifyApiTokenMaxLength.Designer.cs
+++ SGL.Analytics.Backend.Users.Infrastructure/Migrations/20211213172940_UnifyApiTokenMaxLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SGL.Analytics.Backend.Users.Infrastructure.Data;
@@ -9,9 +10,11 @@ using SGL.Analytics.Backend.Users.Infrastructure.Data;
 namespace SGL.Analytics.Backend.Users.Infrastructure.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20211213172940_UnifyApiTokenMaxLength")]
+    partial class UnifyApiTokenMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+		/// <inheritdoc/>
+		protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
