7d5aa39d6f944b67371871fc8f56274059026293
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-12-15 13:00:56 +0100

    Specify validation attributes on DTOs also for properties, not only for constructor parameters.
    Rolled-out the record types into full record definitions instead of the short form to workaround the problem described in the following issues:
    - https://github.com/dotnet/runtime/issues/47602
    - https://github.com/dotnet/runtime/issues/49011

--- SGL.Analytics.DTO/LogMetadataDTO.cs
+++ SGL.Analytics.DTO/LogMetadataDTO.cs
@@ -20,5 +20,26 @@ namespace SGL.Analytics.DTO {
 	/// Specifies the metadata transferred along whith the contents when a client upload a game log file to the server.
 	/// These properties are passed by custom headers, as the request body is already taken by the file content.
 	/// </summary>
-	public record LogMetadataDTO(Guid LogFileId, DateTime CreationTime, DateTime EndTime, [PlainName][StringLength(16)] string NameSuffix, LogContentEncoding LogContentEncoding);
+	public record LogMetadataDTO {
+		public Guid LogFileId { get; init; }
+		public DateTime CreationTime { get; init; }
+		public DateTime EndTime { get; init; }
+		[PlainName]
+		[StringLength(16)]
+		public string NameSuffix { get; init; }
+		public LogContentEncoding LogContentEncoding { get; init; }
+
+		public LogMetadataDTO(Guid logFileId, DateTime creationTime, DateTime endTime,
+			[PlainName][StringLength(16)] string nameSuffix, LogContentEncoding logContentEncoding) =>
+			(LogFileId, CreationTime, EndTime, NameSuffix, LogContentEncoding) = (logFileId, creationTime, endTime, nameSuffix, logContentEncoding);
+
+		public void Deconstruct(out Guid logFileId, out DateTime creationTime, out DateTime endTime,
+			out string nameSuffix, out LogContentEncoding logContentEncoding) {
+			logFileId = LogFileId;
+			creationTime = CreationTime;
+			endTime = EndTime;
+			nameSuffix = NameSuffix;
+			logContentEncoding = LogContentEncoding;
+		}
+	}
 }
--- SGL.Analytics.DTO/UserRegistrationDTO.cs
+++ SGL.Analytics.DTO/UserRegistrationDTO.cs
@@ -7,5 +7,28 @@ namespace SGL.Analytics.DTO {
 	/// <summary>
 	/// Specifies the data transferred from the client to the server when a client attempts to registers a user.
 	/// </summary>
-	public record UserRegistrationDTO([PlainName][StringLength(128, MinimumLength = 1)] string AppName, [PlainName(allowBrackets: true)][StringLength(64, MinimumLength = 1)] string? Username, [StringLength(128, MinimumLength = 8)] string Secret, [property: JsonConverter(typeof(ObjectDictionaryJsonConverter))] Dictionary<string, object?> StudySpecificProperties);
+	public record UserRegistrationDTO {
+		[PlainName]
+		[StringLength(128, MinimumLength = 1)]
+		public string AppName { get; init; }
+		[PlainName(allowBrackets: true)]
+		[StringLength(64, MinimumLength = 1)]
+		public string? Username { get; init; }
+		[StringLength(128, MinimumLength = 8)]
+		public string Secret { get; init; }
+		[JsonConverter(typeof(ObjectDictionaryJsonConverter))]
+		public Dictionary<string, object?> StudySpecificProperties { get; init; }
+
+		public UserRegistrationDTO([PlainName][StringLength(128, MinimumLength = 1)] string appName,
+			[PlainName(allowBrackets: true)][StringLength(64, MinimumLength = 1)] string? username,
+			[StringLength(128, MinimumLength = 8)] string secret, Dictionary<string, object?> studySpecificProperties) =>
+			(AppName, Username, Secret, StudySpecificProperties) = (appName, username, secret, studySpecificProperties);
+
+		public void Deconstruct(out string appName, out string? username, out string secret, out Dictionary<string, object?> studySpecificProperties) {
+			appName = AppName;
+			username = Username;
+			secret = Secret;
+			studySpecificProperties = StudySpecificProperties;
+		}
+	}
 }
