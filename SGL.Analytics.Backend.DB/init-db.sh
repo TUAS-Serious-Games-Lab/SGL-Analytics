5710c966827a96e9bf6c3f03324e24265249c8c4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-01-14 16:02:47 +0100

    Reorganized user and database creation to first create those objects before altering privileges.

--- SGL.Analytics.Backend.DB/init-db.sh
+++ SGL.Analytics.Backend.DB/init-db.sh
@@ -6,8 +6,14 @@ sed -i 's/host all all all md5/host all all all scram-sha-256/' /var/lib/postgre
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EOSQL
 	CREATE USER sgla_users WITH ENCRYPTED PASSWORD '${USERREG_PW}';
 	CREATE USER sgla_users_admin WITH ENCRYPTED PASSWORD '${USERREG_ADM_PW}';
+	CREATE USER sgla_logs WITH ENCRYPTED PASSWORD '${LOGS_PW}';
+	CREATE USER sgla_logs_admin WITH ENCRYPTED PASSWORD '${LOGS_ADM_PW}';
+
 	CREATE DATABASE sgla_users;
+	CREATE DATABASE sgla_logs;
 	REVOKE ALL ON DATABASE sgla_users FROM public;
+	REVOKE ALL ON DATABASE sgla_logs FROM public;
+
 	\c sgla_users
 	REVOKE ALL ON SCHEMA public FROM public;
 	GRANT CONNECT ON DATABASE sgla_users TO sgla_users, sgla_users_admin;
@@ -16,10 +22,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EO
 	ALTER DEFAULT PRIVILEGES FOR USER sgla_users_admin IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO sgla_users;
 	ALTER DEFAULT PRIVILEGES FOR USER sgla_users_admin IN SCHEMA public GRANT ALL ON SEQUENCES TO sgla_users;
 
-	CREATE USER sgla_logs WITH ENCRYPTED PASSWORD '${LOGS_PW}';
-	CREATE USER sgla_logs_admin WITH ENCRYPTED PASSWORD '${LOGS_ADM_PW}';
-	CREATE DATABASE sgla_logs;
-	REVOKE ALL ON DATABASE sgla_logs FROM public;
 	\c sgla_logs
 	REVOKE ALL ON SCHEMA public FROM public;
 	GRANT CONNECT ON DATABASE sgla_logs TO sgla_logs, sgla_logs_admin;
