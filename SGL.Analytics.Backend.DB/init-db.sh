7510bcb5682212d4d50733e45b1ad3eb5f5526c7
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-01-14 13:48:48 +0100

    Limit database privileges of service db users.
    - Service users are not owners of the tables and thus can't do ALTER or DROP statements.
    - Service users don't have CREATE privileges on schemas and databases.
    - For now, service users don't have DELETE privileges as they should only collect or update data.
    - As the service users can't ALTER, DROP, or CREATE, they can't perform the migrations anymore. New <service_name>_admin users have been added to perform these privileged operations.
    - Migration containers now use these <service_name>_admin users to apply db migrations.
    - These users are thus also the new owners of the database tables.

--- SGL.Analytics.Backend.DB/init-db.sh
+++ SGL.Analytics.Backend.DB/init-db.sh
@@ -5,9 +5,26 @@ sed -i 's/host all all all md5/host all all all scram-sha-256/' /var/lib/postgre
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EOSQL
 	CREATE USER sgla_users WITH ENCRYPTED PASSWORD '${USERREG_PW}';
+	CREATE USER sgla_users_admin WITH ENCRYPTED PASSWORD '${USERREG_ADM_PW}';
 	CREATE DATABASE sgla_users;
-	GRANT ALL PRIVILEGES ON DATABASE sgla_users TO sgla_users;
+	REVOKE ALL ON DATABASE sgla_users FROM public;
+	\c sgla_users
+	REVOKE ALL ON SCHEMA public FROM public;
+	GRANT CONNECT ON DATABASE sgla_users TO sgla_users, sgla_users_admin;
+	GRANT USAGE ON SCHEMA public TO sgla_users;
+	GRANT ALL ON SCHEMA public TO sgla_users_admin;
+	ALTER DEFAULT PRIVILEGES FOR USER sgla_users_admin IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO sgla_users;
+	ALTER DEFAULT PRIVILEGES FOR USER sgla_users_admin IN SCHEMA public GRANT ALL ON SEQUENCES TO sgla_users;
+
 	CREATE USER sgla_logs WITH ENCRYPTED PASSWORD '${LOGS_PW}';
+	CREATE USER sgla_logs_admin WITH ENCRYPTED PASSWORD '${LOGS_ADM_PW}';
 	CREATE DATABASE sgla_logs;
-	GRANT ALL PRIVILEGES ON DATABASE sgla_logs TO sgla_logs;
+	REVOKE ALL ON DATABASE sgla_logs FROM public;
+	\c sgla_logs
+	REVOKE ALL ON SCHEMA public FROM public;
+	GRANT CONNECT ON DATABASE sgla_logs TO sgla_logs, sgla_logs_admin;
+	GRANT USAGE ON SCHEMA public TO sgla_logs;
+	GRANT ALL ON SCHEMA public TO sgla_logs_admin;
+	ALTER DEFAULT PRIVILEGES FOR USER sgla_logs_admin IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO sgla_logs;
+	ALTER DEFAULT PRIVILEGES FOR USER sgla_logs_admin IN SCHEMA public GRANT ALL ON SEQUENCES TO sgla_logs;
 EOSQL
