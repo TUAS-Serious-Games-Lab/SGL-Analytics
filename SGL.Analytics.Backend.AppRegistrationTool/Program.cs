3178c2a24dd421ac772a7e52ee137df2b9e63230
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-01 20:16:05 +0200

    Added command verb relabel-recipient to app registration tool to allow changing the label of a recipient key.

--- SGL.Analytics.Backend.AppRegistrationTool/Program.cs
+++ SGL.Analytics.Backend.AppRegistrationTool/Program.cs
@@ -88,6 +88,16 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 			public string KeyId { get; set; }
 		}
 
+		[Verb("relabel-recipient", HelpText = "Assign a new label to the given data recipient key in a registered application.")]
+		public class RelabelRecipientOptions : BaseOptions {
+			[Value(0, MetaName = "APP_NAME", HelpText = "The application in which to relabel the recipient.", Required = true)]
+			public string AppName { get; set; }
+			[Value(1, MetaName = "KEYID", HelpText = "The keyid of the recipient to change the label of.", Required = true)]
+			public string KeyId { get; set; }
+			[Value(2, MetaName = "NEW_LABEL", HelpText = "The new label text.", Required = true)]
+			public string Label { get; set; }
+		}
+
 		async static Task<int> Main(string[] args) => await ((Func<ParserResult<object>, Task<int>>)(res => res.MapResult(
 			async (PushOptions opts) => await PushMain(opts),
 			async (GenerateApiTokenOptions opts) => await GenerateApiTokenMain(opts),
@@ -96,9 +106,11 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 			async (ApplyMigrationsOptions opts) => await ApplyMigrationsMain(opts),
 			async (RemoveRecipientOptions opts) => await RemoveRecipientMain(opts),
 			async (ListRecipientsOptions opts) => await ListRecipientsMain(opts),
+			async (RelabelRecipientOptions opts) => await RelabelRecipientMain(opts),
 			async errs => await DisplayHelp(res, errs)
 			)))(new Parser(c => c.HelpWriter = null).
-			ParseArguments<PushOptions, GenerateApiTokenOptions, RemovePropertyOptions, RemoveApplicationOptions, ApplyMigrationsOptions, RemoveRecipientOptions, ListRecipientsOptions>(args));
+			ParseArguments<PushOptions, GenerateApiTokenOptions, RemovePropertyOptions, RemoveApplicationOptions, ApplyMigrationsOptions,
+				RemoveRecipientOptions, ListRecipientsOptions, RelabelRecipientOptions>(args));
 
 		async static Task<int> DisplayHelp(ParserResult<object> result, IEnumerable<Error> errs) {
 			await Console.Out.WriteLineAsync(HelpText.AutoBuild(result, h => {
@@ -217,6 +229,48 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 					await Console.Out.WriteLineAsync($"\t{r.PublicKeyId}\t{r.Label}\t[couldn't load certificate]");
 				}
 			}
+		}
+
+		async static Task<int> RelabelRecipientMain(RelabelRecipientOptions opts) {
+			using var host = CreateHostBuilder(opts, services => { }).Build();
+			using var scope = host.Services.CreateScope();
+			var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+			try {
+				var keyId = KeyId.Parse(opts.KeyId);
+				var usersApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<ApplicationWithUserProperties, Users.Application.Interfaces.ApplicationQueryOptions>>();
+				var usersApp = await usersApps.GetApplicationByNameAsync(opts.AppName, new Users.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RelabelRecipient("UsersAPI", opts.AppName, logger, keyId, usersApp, opts.Label)) {
+					await usersApps.UpdateApplicationAsync(usersApp!);
+				}
+				var logsApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<Domain.Entity.Application, Logs.Application.Interfaces.ApplicationQueryOptions>>();
+				var logsApp = await logsApps.GetApplicationByNameAsync(opts.AppName, new Logs.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RelabelRecipient("LogsAPI", opts.AppName, logger, keyId, logsApp, opts.Label)) {
+					await logsApps.UpdateApplicationAsync(logsApp!);
+				}
+				return 0;
+			}
+			catch (Exception ex) {
+				logger.LogError(ex, "Failed to relabel recipient.");
+				return 2;
+			}
+		}
+
+		private static bool RelabelRecipient(string apiName, string appName, ILogger<Program> logger, KeyId keyId, Application? app, string label) {
+			if (app != null) {
+				var recipient = app.DataRecipients.SingleOrDefault(r => r.PublicKeyId == keyId);
+				if (recipient != null) {
+					logger.LogInformation("Changing label of {keyId} from application {appName} in {apiName} to \"{label}\"...", recipient.PublicKeyId, appName, apiName, label);
+					recipient.Label = label;
+					return true;
+				}
+				else {
+					logger.LogWarning("Recipient is not present in application {appName} in {apiName}.", appName, apiName);
+				}
+			}
+			else {
+				logger.LogWarning("Application {appName} is not present in {apiName}.", appName, apiName);
+			}
+			return false;
 		}
 
 		async static Task<int> RemoveRecipientMain(RemoveRecipientOptions opts) {
