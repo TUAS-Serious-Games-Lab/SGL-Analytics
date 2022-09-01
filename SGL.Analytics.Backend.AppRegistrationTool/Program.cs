277c53286fdac694ca30f6863eea742691dc7071
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-01 20:02:43 +0200

    Implemented remove-recipient command verb to allow removal of recipient keys through app registration tool.

--- SGL.Analytics.Backend.AppRegistrationTool/Program.cs
+++ SGL.Analytics.Backend.AppRegistrationTool/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using SGL.Analytics.Backend.Domain.Entity;
 using SGL.Analytics.Backend.Logs.Infrastructure;
 using SGL.Analytics.Backend.Logs.Infrastructure.Data;
+using SGL.Analytics.Backend.Users.Application.Interfaces;
 using SGL.Analytics.Backend.Users.Infrastructure;
 using SGL.Analytics.Backend.Users.Infrastructure.Data;
 using SGL.Utilities;
@@ -81,7 +82,10 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 
 		[Verb("remove-recipient", HelpText = "Remove a given data recipient key from a registered application.")]
 		public class RemoveRecipientOptions : BaseOptions {
-			// Not implemented
+			[Value(0, MetaName = "APP_NAME", HelpText = "The application from which to remove the recipient from.", Required = true)]
+			public string AppName { get; set; }
+			[Value(1, MetaName = "KEYID", HelpText = "The keyid of the recipient to remove.", Required = true)]
+			public string KeyId { get; set; }
 		}
 
 		async static Task<int> Main(string[] args) => await ((Func<ParserResult<object>, Task<int>>)(res => res.MapResult(
@@ -216,8 +220,45 @@ namespace SGL.Analytics.Backend.AppRegistrationTool {
 		}
 
 		async static Task<int> RemoveRecipientMain(RemoveRecipientOptions opts) {
-			await Console.Out.WriteLineAsync("This verb is not yet implemented.");
-			return 1;
+			using var host = CreateHostBuilder(opts, services => { }).Build();
+			using var scope = host.Services.CreateScope();
+			var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+			try {
+				var keyId = KeyId.Parse(opts.KeyId);
+				var usersApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<ApplicationWithUserProperties, Users.Application.Interfaces.ApplicationQueryOptions>>();
+				var usersApp = await usersApps.GetApplicationByNameAsync(opts.AppName, new Users.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RemoveRecipient("UsersAPI", opts.AppName, logger, keyId, usersApp)) {
+					await usersApps.UpdateApplicationAsync(usersApp!);
+				}
+				var logsApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<Domain.Entity.Application, Logs.Application.Interfaces.ApplicationQueryOptions>>();
+				var logsApp = await logsApps.GetApplicationByNameAsync(opts.AppName, new Logs.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RemoveRecipient("LogsAPI", opts.AppName, logger, keyId, logsApp)) {
+					await logsApps.UpdateApplicationAsync(logsApp!);
+				}
+				return 0;
+			}
+			catch (Exception ex) {
+				logger.LogError(ex, "Failed to remove recipient.");
+				return 2;
+			}
+		}
+
+		private static bool RemoveRecipient(string apiName, string appName, ILogger<Program> logger, KeyId keyId, Application? app) {
+			if (app != null) {
+				var recipient = app.DataRecipients.SingleOrDefault(r => r.PublicKeyId == keyId);
+				if (recipient != null) {
+					logger.LogInformation("Removing {keyId} from application {appName} in {apiName}...", recipient.PublicKeyId, appName, apiName);
+					app.DataRecipients.Remove(recipient);
+					return true;
+				}
+				else {
+					logger.LogWarning("Recipient is not present in application {appName} in {apiName}.", appName, apiName);
+				}
+			}
+			else {
+				logger.LogWarning("Application {appName} is not present in {apiName}.", appName, apiName);
+			}
+			return false;
 		}
 	}
 }
