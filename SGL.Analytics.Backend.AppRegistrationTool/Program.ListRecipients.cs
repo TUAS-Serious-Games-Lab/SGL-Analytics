3a787758fd031ac40f652c9e76270d613f61fff9
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-01 20:25:10 +0200

    Split Program.cs into one file for each main verb for readability.
    (Except for push, as that is already mainly extracted in PushJob.)

---
+++ SGL.Analytics.Backend.AppRegistrationTool/Program.ListRecipients.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Utilities.Backend.Applications;
+using System;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.AppRegistrationTool {
+	public partial class Program {
+		async static Task<int> ListRecipientsMain(ListRecipientsOptions opts) {
+			using var host = CreateHostBuilder(opts, services => { }).Build();
+			using var scope = host.Services.CreateScope();
+			var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+			try {
+				var usersApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<ApplicationWithUserProperties, Users.Application.Interfaces.ApplicationQueryOptions>>();
+				var usersApp = await usersApps.GetApplicationByNameAsync(opts.AppName, new Users.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (usersApp != null) {
+					await Console.Out.WriteLineAsync("Recipients in UsersAPI:");
+					await PrintRecipients(usersApp);
+				}
+				else {
+					await Console.Out.WriteLineAsync("Not present in UsersAPI.");
+				}
+				await Console.Out.WriteLineAsync();
+				var logsApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<Domain.Entity.Application, Logs.Application.Interfaces.ApplicationQueryOptions>>();
+				var logsApp = await logsApps.GetApplicationByNameAsync(opts.AppName, new Logs.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (logsApp != null) {
+					await Console.Out.WriteLineAsync("Recipients in LogsAPI:");
+					await PrintRecipients(logsApp);
+				}
+				else {
+					await Console.Out.WriteLineAsync("Not present in LogsAPI.");
+				}
+				return 0;
+			}
+			catch (Exception ex) {
+				logger.LogError(ex, "Failed to list recipients.");
+				return 2;
+			}
+		}
+
+		private static async Task PrintRecipients(Application app) {
+			await Console.Out.WriteLineAsync("\tPublic Key Id\t| Label\t| Subject\t| Issuer\t| Not Valid Before\t| Not Valid After\t| Serial Number");
+			foreach (var r in app.DataRecipients) {
+				try {
+					var cert = r.Certificate;
+					await Console.Out.WriteLineAsync($"\t{r.PublicKeyId}\t{r.Label}\t{cert.SubjectDN}\t{cert.IssuerDN}\t{cert.NotBefore}\t{cert.NotAfter}\t{Convert.ToHexString(cert.SerialNumber)}");
+				}
+				catch {
+					await Console.Out.WriteLineAsync($"\t{r.PublicKeyId}\t{r.Label}\t[couldn't load certificate]");
+				}
+			}
+		}
+
+	}
+}
---
+++ SGL.Analytics.Backend.AppRegistrationTool/Program.RelabelRecipient.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SGL.Analytics.Backend.Domain.Entity;
+using SGL.Utilities.Backend.Applications;
+using SGL.Utilities.Crypto.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.AppRegistrationTool {
+	public partial class Program {
+		async static Task<int> RelabelRecipientMain(RelabelRecipientOptions opts) {
+			using var host = CreateHostBuilder(opts, services => { }).Build();
+			using var scope = host.Services.CreateScope();
+			var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+			try {
+				var keyId = KeyId.Parse(opts.KeyId);
+				var usersApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<ApplicationWithUserProperties, Users.Application.Interfaces.ApplicationQueryOptions>>();
+				var usersApp = await usersApps.GetApplicationByNameAsync(opts.AppName, new Users.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RelabelRecipient("UsersAPI", opts.AppName, logger, keyId, usersApp, opts.Label)) {
+					await usersApps.UpdateApplicationAsync(usersApp!);
+				}
+				var logsApps = scope.ServiceProvider.GetRequiredService<IApplicationRepository<Domain.Entity.Application, Logs.Application.Interfaces.ApplicationQueryOptions>>();
+				var logsApp = await logsApps.GetApplicationByNameAsync(opts.AppName, new Logs.Application.Interfaces.ApplicationQueryOptions { FetchRecipients = true });
+				if (RelabelRecipient("LogsAPI", opts.AppName, logger, keyId, logsApp, opts.Label)) {
+					await logsApps.UpdateApplicationAsync(logsApp!);
+				}
+				return 0;
+			}
+			catch (Exception ex) {
+				logger.LogError(ex, "Failed to relabel recipient.");
+				return 2;
+			}
+		}
+
+		private static bool RelabelRecipient(string apiName, string appName, ILogger<Program> logger, KeyId keyId, Application? app, string label) {
+			if (app != null) {
+				var recipient = app.DataRecipients.SingleOrDefault(r => r.PublicKeyId == keyId);
+				if (recipient != null) {
+					logger.LogInformation("Changing label of {keyId} from application {appName} in {apiName} to \"{label}\"...", recipient.PublicKeyId, appName, apiName, label);
+					recipient.Label = label;
+					return true;
+				}
+				else {
+					logger.LogWarning("Recipient is not present in application {appName} in {apiName}.", appName, apiName);
+				}
+			}
+			else {
+				logger.LogWarning("Application {appName} is not present in {apiName}.", appName, apiName);
+			}
+			return false;
+		}
+	}
+}
