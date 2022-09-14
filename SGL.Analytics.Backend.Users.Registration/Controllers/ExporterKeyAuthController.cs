db392e21fe758ff1a3da6c5bd557ee2858ac4d90
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-14 19:00:18 +0200

    Specified rest API routes for key-pair-based exporter authentication (WIP).

---
+++ SGL.Analytics.Backend.Users.Registration/Controllers/ExporterKeyAuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGL.Analytics.DTO;
+using SGL.Utilities.Crypto.Keys;
+using System;
+using System.Threading;
+
+namespace SGL.Analytics.Backend.Users.Registration.Controllers {
+	[Route("api/analytics/user/v1/exporter-key-auth")]
+	[ApiController]
+	public class ExporterKeyAuthController : ControllerBase {
+		[HttpPost("challenge")]
+		public ActionResult<ExporterKeyAuthChallengeDTO> ObtainChallenge(ExporterKeyAuthRequestDTO requestDto, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+
+		[HttpPost("complete")]
+		public ActionResult<ExporterKeyAuthResponseDTO> CompleteChallenge(ExporterKeyAuthSignatureDTO signatureDto, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SGL.Analytics.DTO/ExporterKeyAuthDTOs.cs
@@ -0,0 +1,53 @@
+﻿using SGL.Utilities;
+using SGL.Utilities.Crypto.Keys;
+using SGL.Utilities.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SGL.Analytics.DTO {
+	public class ExporterKeyAuthRequestDTO {
+		[PlainName]
+		[StringLength(128, MinimumLength = 1)]
+		public string AppName { get; private set; }
+		public KeyId KeyId { get; private set; }
+
+		public ExporterKeyAuthRequestDTO(string appName, KeyId keyId) {
+			AppName = appName;
+			KeyId = keyId;
+		}
+	}
+	public class ExporterKeyAuthChallengeDTO {
+		public Guid ChallengeId { get; private set; }
+		[MaxLength(64 * 1024)]
+		public byte[] ChallengeBytes { get; private set; }
+		//TODO Change to enum of supported digests?
+		public string DigestAlgorithmToUse { get; private set; }
+
+		public ExporterKeyAuthChallengeDTO(Guid challengeId, byte[] challengeBytes, string digestAlgorithmToUse) {
+			ChallengeId = challengeId;
+			ChallengeBytes = challengeBytes;
+			DigestAlgorithmToUse = digestAlgorithmToUse;
+		}
+	}
+
+	public class ExporterKeyAuthSignatureDTO {
+		public Guid ChallengeId { get; private set; }
+		[MaxLength(64 * 1024)]
+		public byte[] Signature { get; private set; }
+
+		public ExporterKeyAuthSignatureDTO(Guid challengeId, byte[] signature) {
+			ChallengeId = challengeId;
+			Signature = signature;
+		}
+	}
+
+	public class ExporterKeyAuthResponseDTO {
+		public AuthorizationToken Token { get; private set; }
+
+		public ExporterKeyAuthResponseDTO(AuthorizationToken token) {
+			Token = token;
+		}
+	}
+}
