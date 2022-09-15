a4b4c2ca33005db5e109f6ae8463093dafcb9d72
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-15 16:38:20 +0200

    Specified ChallengeState data to keep between opening and completing a challenge.

---
+++ SGL.Analytics.Backend.Users.Application/Values/ChallengeState.cs
@@ -0,0 +1,27 @@
+﻿using SGL.Utilities.Crypto.Keys;
+using SGL.Utilities.Crypto.Signatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Application.Values {
+	public class ChallengeState {
+		public Guid ChallengeId { get; private set; }
+		public string AppName { get; private set; }
+		public KeyId KeyId { get; private set; }
+		public SignatureDigest DigestAlgorithm { get; private set; }
+		public byte[] ChallengeBytes { get; private set; }
+		public DateTime Timeout { get; private set; }
+
+		public ChallengeState(Guid challengeId, string appName, KeyId keyId, SignatureDigest digestAlgorithm, byte[] challengeBytes, DateTime timeout) {
+			ChallengeId = challengeId;
+			AppName = appName;
+			KeyId = keyId;
+			DigestAlgorithm = digestAlgorithm;
+			ChallengeBytes = challengeBytes;
+			Timeout = timeout;
+		}
+	}
+}
