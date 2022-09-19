b241255426d3e6d540efc14280d889f7b3296b76
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-19 15:10:34 +0200

    Exception cleanup.

---
+++ SGL.Analytics.Backend.Domain/Exceptions/CrptoExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Domain.Exceptions {
+	public class InvalidCryptographicMetadataException : Exception {
+		public InvalidCryptographicMetadataException(string? message, Exception? innerException = null) : base(message, innerException) { }
+	}
+
+	public class MissingRecipientDataKeysForEncryptedDataException : InvalidCryptographicMetadataException {
+		public MissingRecipientDataKeysForEncryptedDataException(string? message, Exception? innerException = null) : base(message, innerException) { }
+	}
+
+	public class EncryptedDataWithoutEncryptionMetadataException : InvalidCryptographicMetadataException {
+		public EncryptedDataWithoutEncryptionMetadataException(string? message, Exception? innerException = null) : base(message, innerException) { }
+	}
+}
