4a344aa4347b736438be5a3829db40e4f805df4b
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-19 15:34:55 +0200

    Fixed warning.

--- SGL.Analytics.Backend.Users.Registration.Tests/UserRegistrationIntegrationTest.cs
+++ SGL.Analytics.Backend.Users.Registration.Tests/UserRegistrationIntegrationTest.cs
@@ -481,7 +481,7 @@ namespace SGL.Analytics.Backend.Users.Registration.Tests {
 				var authenticator = new ExporterKeyPairAuthenticator(client, fixture.ExporterKeyPair, fixture.Services.GetRequiredService<ILogger<ExporterKeyPairAuthenticator>>(), fixture.Random);
 				var token = await authenticator.AuthenticateAsync(fixture.AppName);
 				var (principal, validatedToken) = fixture.TokenValidator.Validate(token.Value);
-				Assert.Equal(fixture.ExporterKeyPair.Public.CalculateId(), principal.GetClaim<KeyId>("keyid", KeyId.TryParse));
+				Assert.Equal(fixture.ExporterKeyPair.Public.CalculateId(), principal.GetClaim<KeyId>("keyid", KeyId.TryParse!));
 				Assert.Equal(fixture.ExporterCertificate.SubjectDN.ToString(), principal.GetClaim("exporter-dn"));
 				Assert.Equal(fixture.AppName, principal.GetClaim("appname"));
 			}
