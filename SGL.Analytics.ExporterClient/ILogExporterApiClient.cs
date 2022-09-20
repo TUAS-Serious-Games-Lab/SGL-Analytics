7c41bd3c837e165912a76d76d2feb785b8b8dbe8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-20 10:39:22 +0200

    Specified interfaces for exporter API clients.

---
+++ SGL.Analytics.ExporterClient/ILogExporterApiClient.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using SGL.Analytics.DTO;
+using SGL.Utilities;
+using SGL.Utilities.Crypto.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.ExporterClient {
+	public interface ILogExporterApiClient : IApiClient {
+		Task<IEnumerable<Guid>> GetLogIdListAsync(CancellationToken ct = default);
+		Task<IEnumerable<DownstreamLogMetadataDTO>> GetMetadataForAllLogsAsync(KeyId? recipientKeyId = null, CancellationToken ct = default);
+		Task<DownstreamLogMetadataDTO> GetLogMetadataByIdAsync(Guid id, KeyId? recipientKeyId = null, CancellationToken ct = default);
+		Task<Stream> GetLogContentByIdAsync(Guid id, CancellationToken ct = default);
+	}
+}
