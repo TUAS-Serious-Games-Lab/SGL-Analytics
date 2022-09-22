cc031c141be343158df4157f4ab02d0088a9491f
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-22 18:17:04 +0200

    Specified core API methods for exporter library.

--- SGL.Analytics.ExporterClient/SglAnalyticsExporter.cs
+++ SGL.Analytics.ExporterClient/SglAnalyticsExporter.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using SGL.Analytics.DTO;
+using SGL.Utilities.Crypto.Certificates;
+using SGL.Utilities.Crypto.Keys;
+using System.Runtime.CompilerServices;
 
 namespace SGL.Analytics.ExporterClient {
 	public partial class SglAnalyticsExporter {
@@ -15,6 +19,23 @@ namespace SGL.Analytics.ExporterClient {
 		/// The <see cref="ILoggerFactory"/> object that this client uses for logging.
 		/// </summary>
 		public ILoggerFactory LoggerFactory { get; }
+		public (KeyId AuthenticationKey, KeyId DecryptionKey, Certificate AuthenticationCertificate, Certificate DecryptionCertificate)? CurrentKeys { get; private set; } = null;
+		public string? CurrentAppName { get; private set; } = null;
 
+		public async Task UseKeyFileAsync(TextReader reader, string sourceName, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+
+		public async Task SwitchToApplicationAsync(string appName, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+
+		public async IAsyncEnumerable<(LogFileMetadata Metadata, Stream Content)> GetDecryptedLogFilesAsync(Func<DownstreamLogMetadataDTO, bool> filter, [EnumeratorCancellation] CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+
+		public async IAsyncEnumerable<UserRegistrationData> GetDecryptedUserRegistrationsAsync(Func<UserMetadataDTO, bool> filter, [EnumeratorCancellation] CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
 	}
 }
