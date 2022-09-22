fc9030b04dddd99495feb631d0d77412304722a7
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-22 10:13:33 +0200

    Specified interfaces for sinks to process decrypted log files and user registration data.

---
+++ SGL.Analytics.ExporterClient/LogFileMetadata.cs
@@ -0,0 +1,57 @@
+﻿using SGL.Analytics.DTO;
+using SGL.Utilities.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.ExporterClient {
+	public class LogFileMetadata {
+		/// <summary>
+		/// The id of the uploaded log file on the client device.
+		/// </summary>
+		public Guid LogFileId { get; private set; }
+		/// <summary>
+		/// The id of the user who uploaded the log file.
+		/// </summary>
+		public Guid UserId { get; private set; }
+		/// <summary>
+		/// The time when recording of the log file was started.
+		/// </summary>
+		public DateTime CreationTime { get; private set; }
+		/// <summary>
+		/// The time when recording of the log file was ended.
+		/// </summary>
+		public DateTime EndTime { get; private set; }
+		/// <summary>
+		/// The time when the log file was uploaded.
+		/// </summary>
+		public DateTime UploadTime { get; private set; }
+		/// <summary>
+		/// The file name suffix for the log file as specified by the client application.
+		/// </summary>
+		public string NameSuffix { get; private set; }
+		/// <summary>
+		/// The encoding of the log file content as specified by the client application.
+		/// This specifies the inner encoding inside the encryption.
+		/// </summary>
+		public LogContentEncoding LogContentEncoding { get; private set; }
+		/// <summary>
+		/// The size of the log file in bytes.
+		/// </summary>
+		public long? Size { get; private set; }
+
+		internal LogFileMetadata(Guid logFileId, Guid userId, DateTime creationTime, DateTime endTime, DateTime uploadTime, string nameSuffix, LogContentEncoding logContentEncoding, long? size) {
+			LogFileId = logFileId;
+			UserId = userId;
+			CreationTime = creationTime;
+			EndTime = endTime;
+			UploadTime = uploadTime;
+			NameSuffix = nameSuffix;
+			LogContentEncoding = logContentEncoding;
+			Size = size;
+		}
+	}
+}
---
+++ SGL.Analytics.ExporterClient/UserRegistrationData.cs
@@ -0,0 +1,30 @@
+﻿using SGL.Utilities.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.ExporterClient {
+	public class UserRegistrationData {
+		/// <summary>
+		/// The unique id identifying the user.
+		/// </summary>
+		public Guid UserId { get; private set; }
+		/// <summary>
+		/// The Username for the user, can be the string form of the id if none was given during the registration.
+		/// </summary>
+		public string Username { get; private set; }
+
+		public IReadOnlyDictionary<string, object?> StudySpecificProperties { get; private set; }
+		public IReadOnlyDictionary<string, object?> EncryptedStudySpecificProperties { get; private set; }
+
+		internal UserRegistrationData(Guid userId, string username, IReadOnlyDictionary<string, object?> studySpecificProperties, IReadOnlyDictionary<string, object?> encryptedStudySpecificProperties) {
+			UserId = userId;
+			Username = username;
+			StudySpecificProperties = studySpecificProperties;
+			EncryptedStudySpecificProperties = encryptedStudySpecificProperties;
+		}
+	}
+}
