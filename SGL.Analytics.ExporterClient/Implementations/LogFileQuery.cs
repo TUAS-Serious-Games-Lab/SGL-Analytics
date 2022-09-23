d0962b9f8fe8d7b70e92e889e43683ae4d3f7dc8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-23 17:12:35 +0200

    Specified ILogFileQuery.

--- SGL.Analytics.ExporterClient/Implementations/LogFileQuery.cs
+++ SGL.Analytics.ExporterClient/Implementations/LogFileQuery.cs
@@ -6,5 +6,32 @@ using System.Threading.Tasks;
 
 namespace SGL.Analytics.ExporterClient {
 	internal class LogFileQuery : ILogFileQuery {
+		public ILogFileQuery EndedAfter(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery EndedBefore(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery StartedAfter(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery StartedBefore(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery UploadedAfter(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery UploadedBefore(DateTime timestamp) {
+			throw new NotImplementedException();
+		}
+
+		public ILogFileQuery UserOneOf(IEnumerable<Guid> userIds) {
+			throw new NotImplementedException();
+		}
 	}
 }
--- SGL.Analytics.ExporterClient/Interfaces/ILogFileQuery.cs
+++ SGL.Analytics.ExporterClient/Interfaces/ILogFileQuery.cs
@@ -1,4 +1,11 @@
 ﻿namespace SGL.Analytics.ExporterClient {
 	public interface ILogFileQuery {
+		ILogFileQuery StartedAfter(DateTime timestamp);
+		ILogFileQuery StartedBefore(DateTime timestamp);
+		ILogFileQuery EndedAfter(DateTime timestamp);
+		ILogFileQuery EndedBefore(DateTime timestamp);
+		ILogFileQuery UploadedAfter(DateTime timestamp);
+		ILogFileQuery UploadedBefore(DateTime timestamp);
+		ILogFileQuery UserOneOf(IEnumerable<Guid> userIds);
 	}
 }
