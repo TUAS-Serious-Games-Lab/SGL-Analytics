2944830b4ee0d672e71fe7c186d544f10201532f
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-05-16 17:13:28 +0200

    Added stub for rekeying controller to user registrations service.

---
+++ SGL.Analytics.Backend.Users.Registration/Controllers/RekeyingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SGL.Utilities.Crypto.EndToEnd;
+using SGL.Utilities.Crypto.Keys;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Users.Registration.Controllers {
+	[Route("api/analytics/user/v1/rekey")]
+	[ApiController]
+	[Authorize(Policy = "ExporterUser")]
+	public class RekeyingController : ControllerBase {
+		[HttpGet("{keyId}")]
+		public async Task<ActionResult<Dictionary<Guid, EncryptionInfo>>> GetKeysForRekeying([FromRoute(Name = "keyId")] KeyId recipientKeyId, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+
+		[HttpPut("{keyId}")]
+		public async Task<ActionResult> PutRekeyedKeys([FromRoute(Name = "keyId")] KeyId recipientKeyId, [FromBody] Dictionary<Guid, DataKeyInfo> dataKeys, CancellationToken ct = default) {
+			throw new NotImplementedException();
+		}
+	}
+}
