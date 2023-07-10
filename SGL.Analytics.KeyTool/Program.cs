17fa7e60adb2707d1d337f1954a84628a7919831
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-07-10 14:37:19 +0200

    Workaround for unwanted console window due to https://github.com/dotnet/runtime/issues/3828

--- SGL.Analytics.KeyTool/Program.cs
+++ SGL.Analytics.KeyTool/Program.cs
@@ -1,10 +1,22 @@
+using System.Runtime.InteropServices;
+
 namespace SGL.Analytics.KeyTool {
 	internal static class Program {
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		const int SW_HIDE = 0;
+
 		/// <summary>
 		///  The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			// Workaround for https://github.com/dotnet/runtime/issues/3828
+			var handle = GetConsoleWindow();
+			ShowWindow(handle, SW_HIDE);
+
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
