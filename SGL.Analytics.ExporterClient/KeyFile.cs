a0b497b92d140dbbfa93605a4bdf464345b151bd
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-07-13 17:17:45 +0200

    Added API docs for KeyFile class.

--- SGL.Analytics.ExporterClient/KeyFile.cs
+++ SGL.Analytics.ExporterClient/KeyFile.cs
@@ -10,12 +10,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.ExporterClient {
+	/// <summary>
+	/// Encalpsulates the cryptographic objects in a recipient user key file and the funtionality for loading such files.
+	/// </summary>
 	public class KeyFile {
+		/// <summary>
+		/// The certificate attesting the purpose of <see cref="AuthenticationKeyPair"/>.
+		/// </summary>
 		public Certificate AuthenticationCertificate { get; set; }
+		/// <summary>
+		/// The certificate attesting the purpose of <see cref="RecipientKeyPair"/>.
+		/// </summary>
 		public Certificate RecipientCertificate { get; set; }
+		/// <summary>
+		/// The asymmetric key pair used for authenticating the user against the backend API using a signature challenge.
+		/// </summary>
 		public KeyPair AuthenticationKeyPair { get; set; }
+		/// <summary>
+		/// The asymmetric key pair used for decrypting data keys of end-to-end encrypted data objects.
+		/// </summary>
 		public KeyPair RecipientKeyPair { get; set; }
+		/// <summary>
+		/// The key id of <see cref="AuthenticationKeyPair"/>.
+		/// </summary>
 		public KeyId AuthenticationKeyId { get; set; }
+		/// <summary>
+		/// The key id of <see cref="RecipientKeyPair"/>.
+		/// </summary>
 		public KeyId RecipientKeyId { get; set; }
 
 		private KeyFile(Certificate authenticationCertificate, Certificate recipientCertificate, KeyPair authenticationKeyPair, KeyPair recipientKeyPair, KeyId authenticationKeyId, KeyId recipientKeyId) {
@@ -27,16 +48,42 @@ namespace SGL.Analytics.ExporterClient {
 			RecipientKeyId = recipientKeyId;
 		}
 
+		/// <summary>
+		/// An overload of <see cref="Load(TextReader, string, Func{char[]}, ILogger, CancellationToken)"/> without the logger.
+		/// </summary>
 		public static KeyFile Load(TextReader reader, string sourceName, Func<char[]> getPassword, CancellationToken ct = default) =>
 			Load(reader, sourceName, getPassword, NullLogger.Instance, ct);
+		/// <summary>
+		/// Synchronously loads key file data from PEM data in <paramref name="reader"/>.
+		/// </summary>
+		/// <param name="reader">A reader providing the encoded PEM data.</param>
+		/// <param name="sourceName">A name referring to <paramref name="reader"/>, e.g. the name of the file that <paramref name="reader"/> reads.</param>
+		/// <param name="getPassword">A function object used to obtain the password / passphrase for encrypted private keys.</param>
+		/// <param name="logger">A logger object used for logging problems during the loading process.</param>
+		/// <param name="ct">An optional cancellation token to allow cancelling the operation. </param>
+		/// <returns>An object containing the mapped cryptographic objects in the key file.</returns>
+		/// <exception cref="KeyFileException">If the key file couldn't be loaded correctly.</exception>
 		public static KeyFile Load(TextReader reader, string sourceName, Func<char[]> getPassword, ILogger logger, CancellationToken ct = default) {
 			var pemReader = new PemObjectReader(reader, getPassword);
 			var readData = InnerLoad(pemReader, sourceName, logger, ct);
 			return new KeyFile(readData.AuthenticationCertificate, readData.RecipientCertificate, readData.AuthenticationKeyPair, readData.RecipientKeyPair, readData.AuthenticationKeyId, readData.RecipientKeyId);
 		}
 
+		/// <summary>
+		/// An overload of <see cref="LoadAsync(TextReader, string, Func{char[]}, ILogger, CancellationToken)"/> without the logger.
+		/// </summary>
 		public static Task<KeyFile> LoadAsync(TextReader reader, string sourceName, Func<char[]> getPassword, CancellationToken ct = default) =>
 			LoadAsync(reader, sourceName, getPassword, NullLogger.Instance, ct);
+		/// <summary>
+		/// Asynchronously loads key file data from PEM data in <paramref name="reader"/>.
+		/// </summary>
+		/// <param name="reader">A reader providing the encoded PEM data.</param>
+		/// <param name="sourceName">A name referring to <paramref name="reader"/>, e.g. the name of the file that <paramref name="reader"/> reads.</param>
+		/// <param name="getPassword">A function object used to obtain the password / passphrase for encrypted private keys.</param>
+		/// <param name="logger">A logger object used for logging problems during the loading process.</param>
+		/// <param name="ct">An optional cancellation token to allow cancelling the operation. </param>
+		/// <returns>An object containing the mapped cryptographic objects in the key file.</returns>
+		/// <exception cref="KeyFileException">If the key file couldn't be loaded correctly.</exception>
 		public static async Task<KeyFile> LoadAsync(TextReader reader, string sourceName, Func<char[]> getPassword, ILogger logger, CancellationToken ct = default) {
 			var pemReader = new PemObjectReader(reader, getPassword);
 			var readData = await Task.Run(() => InnerLoad(pemReader, sourceName, logger, ct));
