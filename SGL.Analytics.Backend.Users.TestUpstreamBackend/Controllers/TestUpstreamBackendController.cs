a2391df073db57dc6335246f22e91a35a7018fc8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-08-14 20:02:49 +0200

    Created TestUpstreamBackend service as a test stub for testing delegated authentication.

---
+++ SGL.Analytics.Backend.Users.TestUpstreamBackend/Controllers/TestUpstreamBackendController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SGL.Analytics.Backend.Users.Application.DTO;
+using SGL.Analytics.DTO;
+using SGL.Utilities;
+using SGL.Utilities.Backend.Security;
+
+namespace SGL.Analytics.Backend.Users.TestUpstreamBackend.Controllers {
+	[Route("api/analytics/test/upstream/v1")]
+	[ApiController]
+	public class TestUpstreamBackendController : ControllerBase {
+		private readonly ILogger<TestUpstreamBackendController> logger;
+		private readonly TestUpstreamBackendOptions options;
+		private readonly IExplicitTokenService explicitTokenService;
+
+		public TestUpstreamBackendController(IOptions<TestUpstreamBackendOptions> options, ILogger<TestUpstreamBackendController> logger) {
+			this.options = options.Value;
+			this.logger = logger;
+		}
+
+		[HttpPost("start-session")]
+		public async Task<ActionResult<LoginResponseDTO>> StartSession([FromBody] string secret, CancellationToken ct = default) {
+			if (string.IsNullOrWhiteSpace(secret) || secret.Length < 10) {
+				return BadRequest("Bad secret.");
+			}
+			if (options.Secret != secret) {
+				return Unauthorized("Incorrect secret.");
+			}
+			else {
+				var fakeUserId = Guid.NewGuid();
+				var token = explicitTokenService.IssueAuthenticationToken(("userid", $"{fakeUserId:D}"), ("appname", options.AppName));
+				return new LoginResponseDTO(token.Token, fakeUserId, token.Expiry);
+			}
+		}
+
+		[Authorize]
+		[HttpPost("check-token")]
+		public async Task<ActionResult<UpstreamTokenCheckResponse>> CheckToken([FromBody] UpstreamTokenCheckRequest request, CancellationToken ct = default) {
+			var tokenUserId = HttpContext.User.GetClaim<Guid>("userid", Guid.TryParse);
+			var tokenAppName = HttpContext.User.GetClaimOrNull("appname");
+			var tokenExpiry = User.GetClaim("exp", (string s, out DateTime p) => {
+				if (int.TryParse(s, out int timestamp)) {
+					p = DateTime.UnixEpoch.AddSeconds(timestamp);
+					return true;
+				}
+				p = default;
+				return false;
+			});
+			if (options.AppName != tokenAppName) {
+				return Unauthorized("Invalid token.");
+			}
+			if (tokenAppName != request.RequestingAppName) {
+				return Unauthorized("Token doesn't match requesting app.");
+			}
+			return new UpstreamTokenCheckResponse(tokenUserId, tokenExpiry);
+		}
+	}
+}
---
+++ SGL.Analytics.Backend.Users.TestUpstreamBackend/TestUpstreamBackendOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace SGL.Analytics.Backend.Users.TestUpstreamBackend {
+	public class TestUpstreamBackendOptions {
+		public string AppName { get; set; } = "Testing";
+		public string Secret { get; set; } = null!;
+	}
+}
