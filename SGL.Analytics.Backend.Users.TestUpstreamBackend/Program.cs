9f25b3d98deab1c660af7b0386036979a5d3497b
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-08-15 16:03:04 +0200

    Ported Startup class to new builder style as UseStartup does no longer work with WebApplicationBuilder.

--- SGL.Analytics.Backend.Users.TestUpstreamBackend/Program.cs
+++ SGL.Analytics.Backend.Users.TestUpstreamBackend/Program.cs
@@ -1,5 +1,6 @@
-
+using Prometheus;
 using SGL.Utilities.Backend.AspNetCore;
+using SGL.Utilities.Backend.Security;
 using SGL.Utilities.Logging.FileLogging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,16 +17,45 @@ builder.Logging.AddFile(builder => {
 	builder.AddUserIdScopePlaceholder();
 	builder.AddAppNameScopePlaceholder();
 });
-builder.WebHost.UseStartup<Startup>();
+builder.Services.Configure<FileLoggingProviderOptions>(config => {
+	config.Constants.TryAdd("ServiceName", "SGL.Analytics.Test.Upstream");
+});
+
+builder.Services.UseJwtLoginService(builder.Configuration);
+builder.Services.UseJwtExplicitTokenService(builder.Configuration);
+builder.Services.UseJwtBearerAuthentication(builder.Configuration);
+builder.Services.AddAuthorization(options => {
+	options.AddPolicy("AuthenticatedAppUser", p => p.RequireClaim("userid").RequireClaim("appname"));
+	options.DefaultPolicy = options.GetPolicy("AuthenticatedAppUser") ?? throw new InvalidOperationException("Couldn't find AuthenticatedAppUser policy.");
+});
+
+builder.Services.AddHealthChecks().ForwardToPrometheus();
+
+DiagnosticSourceAdapter.StartListening();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+	app.UseDeveloperExceptionPage();
+}
+else {
+	app.UseLoggingExceptionHandler<Program>();
+}
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseHttpMetrics();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => {
+	endpoints.MapControllers();
+	endpoints.MapHealthChecks("/health").RequireHost($"localhost:{app.Configuration["ManagementPort"]}");
+	endpoints.MapMetrics().RequireHost($"*:{app.Configuration["ManagementPort"]}");
+});
 
 app.Run();
