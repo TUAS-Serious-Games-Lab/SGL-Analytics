1a0176ada4b58b37c5bdd1c2cd55ae11f1650f47
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-10-24 16:01:44 +0200

    Added composite sink classes to allow combining multiple sinks to have all of them receive decrypted data.

---
+++ SGL.Analytics.ExporterClient/Implementations/CompositeLogFileSink.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.ExporterClient.Implementations {
+	/// <summary>
+	/// Provides an <see cref="ILogFileSink"/> implementation that delegates to arbitrarily many other sink objects
+	/// to allow using multiple sink objects on a <see cref="SglAnalyticsExporter.GetDecryptedLogFilesAsync(ILogFileSink, Func{ILogFileQuery, ILogFileQuery}, CancellationToken)"/>
+	/// call, that should all receive the decrypted data.
+	/// The <see cref="ILogFileSink.ProcessLogFileAsync(LogFileMetadata, Stream?, CancellationToken)"/>
+	/// methods on constituent sinks can be invoked sequentially or concurrently, controlled by <see cref="RunConcurrently"/>.
+	/// Incoming log file contents are cached in memory to allow each sink to read the content from the start,
+	/// even though the original stream object is not necessarily rewindable.
+	/// </summary>
+	public class CompositeLogFileSink : ILogFileSink {
+		/// <summary>
+		/// Constructs a composite sink with the list of constituent sinks given in <paramref name="innerSinks"/>.
+		/// </summary>
+		/// <param name="innerSinks">The list of sinks to which calls shall be delegated.</param>
+		/// <param name="runConcurrently">True if the sink shall invoke the <paramref name="innerSinks"/> concurrently on the thread pool.
+		/// False if they shall be invoked sequentially.</param>
+		public CompositeLogFileSink(IList<ILogFileSink> innerSinks, bool runConcurrently = false) {
+			InnerSinks = innerSinks;
+			RunConcurrently = runConcurrently;
+		}
+
+		/// <summary>
+		/// True if the sink invokes the <see cref="InnerSinks"/> concurrently on the thread pool.
+		/// False if they are invoked sequentially.
+		/// </summary>
+		public bool RunConcurrently { get; }
+
+		/// <summary>
+		/// The list of sinks to which calls shall be delegated.
+		/// </summary>
+		public IList<ILogFileSink> InnerSinks { get; }
+
+		/// <summary>
+		/// Implements <see cref="ILogFileSink.ProcessLogFileAsync(LogFileMetadata, Stream?, CancellationToken)"/> 
+		/// by delegating each call to all sinks in <see cref="InnerSinks"/>.
+		/// </summary>
+		public async Task ProcessLogFileAsync(LogFileMetadata metadata, Stream? content, CancellationToken ct) {
+			byte[] contentBuffer = Array.Empty<byte>();
+			if (content != null) {
+				using (var inputBuffer = new MemoryStream()) {
+					await content.CopyToAsync(inputBuffer, ct);
+					contentBuffer = inputBuffer.ToArray();
+				}
+			}
+			if (RunConcurrently) {
+				await Task.WhenAll(InnerSinks.Select(innerSink => {
+					if (content != null) {
+						using var contentStream = new MemoryStream(contentBuffer, writable: false);
+						return Task.Run(() => innerSink.ProcessLogFileAsync(metadata, contentStream, ct), ct);
+					}
+					else {
+						return Task.Run(() => innerSink.ProcessLogFileAsync(metadata, null, ct), ct);
+					}
+				}));
+			}
+			else {
+				foreach (var innerSink in InnerSinks) {
+					ct.ThrowIfCancellationRequested();
+					if (content != null) {
+						using var contentStream = new MemoryStream(contentBuffer, writable: false);
+						await innerSink.ProcessLogFileAsync(metadata, contentStream, ct);
+					}
+					else {
+						await innerSink.ProcessLogFileAsync(metadata, null, ct);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SGL.Analytics.ExporterClient/Implementations/CompositeUserRegistrationSink.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.ExporterClient.Implementations {
+	/// <summary>
+	/// Provides an <see cref="IUserRegistrationSink"/> implementation that delegates to arbitrarily many other sink objects
+	/// to allow using multiple sink objects on a <see cref="SglAnalyticsExporter.GetDecryptedUserRegistrationsAsync(IUserRegistrationSink, Func{IUserRegistrationQuery, IUserRegistrationQuery}, CancellationToken)"/>
+	/// call, that should all receive the decrypted data.
+	/// The <see cref="IUserRegistrationSink.ProcessUserRegistrationAsync(UserRegistrationData, CancellationToken)"/>
+	/// methods on constituent sinks can be invoked sequentially or concurrently, controlled by <see cref="RunConcurrently"/>.
+	/// </summary>
+	public class CompositeUserRegistrationSink : IUserRegistrationSink {
+		/// <summary>
+		/// Constructs a composite sink with the list of constituent sinks given in <paramref name="innerSinks"/>.
+		/// </summary>
+		/// <param name="innerSinks">The list of sinks to which calls shall be delegated.</param>
+		/// <param name="runConcurrently">True if the sink shall invoke the <paramref name="innerSinks"/> concurrently on the thread pool.
+		/// False if they shall be invoked sequentially.</param>
+		public CompositeUserRegistrationSink(IList<IUserRegistrationSink> innerSinks, bool runConcurrently = false) {
+			InnerSinks = innerSinks;
+			RunConcurrently = runConcurrently;
+		}
+
+		/// <summary>
+		/// True if the sink invokes the <see cref="InnerSinks"/> concurrently on the thread pool.
+		/// False if they are invoked sequentially.
+		/// </summary>
+		public bool RunConcurrently { get; }
+		/// <summary>
+		/// The list of sinks to which calls shall be delegated.
+		/// </summary>
+		public IList<IUserRegistrationSink> InnerSinks { get; }
+
+		/// <summary>
+		/// Implements <see cref="IUserRegistrationSink.ProcessUserRegistrationAsync(UserRegistrationData, CancellationToken)"/> 
+		/// by delegating each call to all sinks in <see cref="InnerSinks"/>.
+		/// </summary>
+		public async Task ProcessUserRegistrationAsync(UserRegistrationData userRegistrationData, CancellationToken ct) {
+			if (RunConcurrently) {
+				await Task.WhenAll(InnerSinks.Select(innerSink =>
+					Task.Run(() => innerSink.ProcessUserRegistrationAsync(userRegistrationData, ct), ct)));
+			}
+			else {
+				foreach (var innerSink in InnerSinks) {
+					await innerSink.ProcessUserRegistrationAsync(userRegistrationData, ct);
+				}
+			}
+		}
+	}
+}
